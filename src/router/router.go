3933904e8bcf9b51cea52b1741075e6b62e8c90d
Travis <treeder@gmail.com>
2013-01-25 18:36:41 -0800

    not deploying

--- src/router/router.go
+++ src/router/router.go
@@ -95,6 +95,7 @@ func main() {
 	if err != nil {
 		log.Panicln(err)
 	}
+	// recompile?
 	err = session.DB(config.MongoAuth.Database).Login(config.MongoAuth.Username, config.MongoAuth.Password)
 	if err != nil {
 		log.Fatalln("Could not log in to db:", err)
