f5deb84115421350c49a05626a874e3d8e087a20
Evan Shaw <edsrzf@gmail.com>
2016-07-01 09:28:39 +1200

    gofmt; add functions to .gitignore

--- checkroute/checkroute.go
+++ checkroute/checkroute.go
@@ -1,9 +1,8 @@
-
 package main
 
 import (
-	"github.com/iron-io/iron_go/cache"
 	"github.com/iron-io/common"
+	"github.com/iron-io/iron_go/cache"
 
 	"encoding/json"
 	"log"
@@ -11,19 +10,18 @@ import (
 
 var config struct {
 	Iron struct {
-	Token      string `json:"token"`
-	ProjectId  string `json:"project_id"`
-	SuperToken string `json:"super_token"`
-	Host 	   string `json:"host"`
-} `json:"iron"`
+		Token      string `json:"token"`
+		ProjectId  string `json:"project_id"`
+		SuperToken string `json:"super_token"`
+		Host       string `json:"host"`
+	} `json:"iron"`
 	MongoAuth common.MongoConfig `json:"mongo_auth"`
 	Logging   struct {
-	To     string `json:"to"`
-	Level  string `json:"level"`
-	Prefix string `json:"prefix"`
+		To     string `json:"to"`
+		Level  string `json:"level"`
+		Prefix string `json:"prefix"`
+	}
 }
-}
-
 
 type Route struct {
 	// TODO: Change destinations to a simple cache so it can expire entries after 55 minutes (the one we use in common?)
@@ -34,10 +32,9 @@ type Route struct {
 	CodeName     string   `json:"code_name"`
 }
 
-
 var icache = cache.New("routing-table")
 
-func main(){
+func main() {
 	log.Println("STARTING")
 
 	var configFile string
@@ -46,35 +43,32 @@ func main(){
 	configFile = "config_" + env + ".json"
 	common.LoadConfig("iron_mq", configFile, &config)
 	common.SetLogLevel(config.Logging.Level)
-//	common.SetLogLocation(config.Logging.To, config.Logging.Prefix)
+	//	common.SetLogLocation(config.Logging.To, config.Logging.Prefix)
 
 	icache.Settings.UseConfigMap(map[string]interface{}{
-		"token": config.Iron.Token,
+		"token":      config.Iron.Token,
 		"project_id": config.Iron.ProjectId,
-		"host": config.Iron.Host,
+		"host":       config.Iron.Host,
 	})
 	log.Println("icache settings:", icache.Settings)
 
 	host := "routertest5.irondns.info"
 
-//	r2 := Route{
-//		Host: host,
-//	}
-//	err2 := putRoute(&r2)
-//	log.Println("err2:", err2)
+	//	r2 := Route{
+	//		Host: host,
+	//	}
+	//	err2 := putRoute(&r2)
+	//	log.Println("err2:", err2)
 
 	log.Println("CHECKING ROUTE")
 	route, err := getRoute(host)
 	log.Println("route:", route)
 	log.Println("err:", err)
 
-//	err = icache.Delete(host)
-//	log.Println("delete err:", err)
-
+	//	err = icache.Delete(host)
+	//	log.Println("delete err:", err)
 
 }
-
-
 
 func getRoute(host string) (*Route, error) {
 	rx, err := icache.Get(host)
@@ -89,7 +83,6 @@ func getRoute(host string) (*Route, error) {
 	}
 	return &route, err
 }
-
 
 func putRoute(route *Route) error {
 	item := cache.Item{}
