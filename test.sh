b21029c3e35444a41a768a26dfa2f36395af0b0a
Pedro Nasser <pedronasser@gmail.com>
2016-09-12 14:52:40 -0300

    update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,7 @@
 export GO15VENDOREXPERIMENT=1
-export IGNORE_MEMORY=1
 
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock \
-    -e TEST_DOCKER_USERNAME \
-    -e TEST_DOCKER_PASSWORD \
-    -e CI="$CI" \
+    -e IGNORE_MEMORY=1 \
     -e LOG_LEVEL=debug \
     -e GOPATH="$PWD/../../../.." \
     -v "$PWD":"$PWD" -w "$PWD" iron/go-dind go test -v $(go list ./... | grep -v /vendor/)
