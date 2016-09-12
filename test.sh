6f9432b7ead85dc5a8ba8eb9a789acb99209c5d5
Pedro Nasser <pedronasser@gmail.com>
2016-09-12 14:31:50 -0300

    update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
-./build.sh
-
 export GO15VENDOREXPERIMENT=1
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock \
+docker run --rm -it -m 2G -v /var/run/docker.sock:/var/run/docker.sock \
     -e TEST_DOCKER_USERNAME \
     -e TEST_DOCKER_PASSWORD \
     -e CI="$CI" \
