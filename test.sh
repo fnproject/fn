86b0ab3c9f958efda87304da1d874c84ab8daacc
Pedro Nasser <pedronasser@gmail.com>
2016-09-12 11:45:50 -0300

    fix CI

---
+++ test.sh
@@ -0,0 +1,10 @@
+./build.sh
+
+export GO15VENDOREXPERIMENT=1
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock \
+    -e TEST_DOCKER_USERNAME \
+    -e TEST_DOCKER_PASSWORD \
+    -e CI="$CI" \
+    -e LOG_LEVEL=debug \
+    -e GOPATH="$PWD/../../../.." \
+    -v "$PWD":"$PWD" -w "$PWD" iron/go-dind go test -v $(go list ./... | grep -v /vendor/)
