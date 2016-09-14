28fb87e5c78a4ae57712635e6875ac45868ae509
Pedro Nasser <pedronasser@gmail.com>
2016-09-13 23:36:11 -0300

    remove examples from test

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock \
     -e IGNORE_MEMORY=1 \
     -e LOG_LEVEL=debug \
     -e GOPATH="$PWD/../../../.." \
-    -v "$PWD":"$PWD" -w "$PWD" iron/go-dind go test -v $(go list ./... | grep -v /vendor/)
+    -v "$PWD":"$PWD" -w "$PWD" iron/go-dind go test -v $(go list ./... | grep -v /vendor/ | grep -v /examples/)
