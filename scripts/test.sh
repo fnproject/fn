15e76965a61d0416af348d9a7b58a7fd83bd341f
C Cirello <ccirello@users.noreply.github.com>
2016-10-12 19:40:47 +0200

    Remove examples/ directory from test scope (#147)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --privileged --rm -e LOG_LEVEL=debug -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD":/go/src/github.com/iron-io/functions -w /go/src/github.com/iron-io/functions iron/go:dev go test -v $(glide nv)
+docker run -ti --privileged --rm -e LOG_LEVEL=debug -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD":/go/src/github.com/iron-io/functions -w /go/src/github.com/iron-io/functions iron/go:dev go test -v $(glide nv | grep -v examples)
