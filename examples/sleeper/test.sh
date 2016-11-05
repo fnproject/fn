a61c4dab789fe92d6f0ad14e30e91a522629e9d6
C Cirello <ccirello@users.noreply.github.com>
2016-11-05 15:33:07 -0700

    Assert License (#224)
    * license: assert license for Go code
    * license: add in shell scripts
    * license: assert license for Ruby code
    * license: assert license to individual cases
    * license: assert license to Dockerfile

--- examples/sleeper/test.sh
+++ examples/sleeper/test.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+
+# Copyright 2016 Iron.io
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -x
 
 ./build.sh
 
-PAYLOAD='{"sleep": 5}' 
+PAYLOAD='{"sleep": 5}'
 
 # test it
 echo $PAYLOAD | docker run --rm -i -e TEST=1 iron/func-sleeper
