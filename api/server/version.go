92950d182efb656d515f2f29b5ad14dcafb85220
Ubuntu <ubuntu@box1050.localdomain>
2016-11-06 17:18:39 +0000

    functions: 0.0.57 release [skip ci]

--- api/server/version.go
+++ api/server/version.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version of IronFunctions
-var Version = "0.0.56"
+var Version = "0.0.57"
 
 func handleVersion(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"version": Version})
