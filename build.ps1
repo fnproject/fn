cf299239e87f1349c82f7b30d37e574d2c64952e
Travis Reeder <treeder@gmail.com>
2016-11-09 09:40:35 -0800

    Added build and run commands for PowerShell (#244)

--- build.ps1
+++ build.ps1
@@ -1,8 +1,21 @@
 # Build script for PowerShell
 $ErrorActionPreference = "Stop"
 
-$pwd = (Resolve-Path .\).Path
-Write-Host "pwd: " $pwd
+$cmd = $args[0]
+Write-Host "cmd: $cmd"
 
-docker run --rm -v ${pwd}:/go/src/github.com/iron-io/functions -w /go/src/github.com/iron-io/functions iron/go:dev go build -o functions-alpine
-docker build -t iron/functions:latest .
+function build () {
+    docker run --rm -v ${pwd}:/go/src/github.com/iron-io/functions -w /go/src/github.com/iron-io/functions iron/go:dev go build -o functions-alpine
+    docker build -t iron/functions:latest .
+}
+
+function run () {
+    docker run --rm --privileged -it -e LOG_LEVEL=debug -e "DB=bolt:///app/data/bolt.db" -v ${pwd}/data:/app/data -p 8080:8080 iron/functions
+}
+
+switch ($cmd)
+{
+    "build" { build }
+    "run" {run}
+    default {"Invalid command: $cmd"}
+}
