135b0bcd8d85896d42727fa2535cc45b771f7fea
CircleCI <circleci@iron.io>
2017-01-18 21:12:53 +0000

    functions: 0.1.100 release [skip ci]

--- api/server/version.go
+++ api/server/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version of IronFunctions
-var Version = "0.1.99"
+var Version = "0.1.100"
 
 func handleVersion(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"version": Version})
--- fn/install.sh
+++ fn/install.sh
@@ -3,7 +3,7 @@ set -e
 
 # Install script to install fn
 
-release="0.1.99"
+release="0.1.100"
 
 command_exists() {
   command -v "$@" > /dev/null 2>&1
