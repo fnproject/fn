bdc175ae093ab060bd2e3c884befb691eeb0beec
Travis Reeder <treeder@gmail.com>
2017-03-23 12:09:27 -0700

    functions: 0.2.52 release [skip ci]

--- api/version/version.go
+++ api/version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // Version of IronFunctions
-var Version = "0.2.51"
+var Version = "0.2.52"
--- fn/install.sh
+++ fn/install.sh
@@ -3,7 +3,7 @@ set -e
 
 # Install script to install fn
 
-release="0.2.51"
+release="0.2.52"
 
 command_exists() {
   command -v "$@" > /dev/null 2>&1
