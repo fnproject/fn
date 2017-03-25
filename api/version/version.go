a036563b9ffa33145e008e951e7a2b314b288823
CircleCI <circleci@iron.io>
2017-03-25 17:35:52 +0000

    functions: 0.2.56 release [skip ci]

--- api/version/version.go
+++ api/version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // Version of IronFunctions
-var Version = "0.2.55"
+var Version = "0.2.56"
--- fn/install.sh
+++ fn/install.sh
@@ -3,7 +3,7 @@ set -e
 
 # Install script to install fn
 
-release="0.2.55"
+release="0.2.56"
 
 command_exists() {
   command -v "$@" > /dev/null 2>&1
