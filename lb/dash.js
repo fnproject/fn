5e69174523420693fca9058037c95bd2211ab494
Reed Allman <rdallman10@gmail.com>
2017-05-22 12:33:57 -0700

    dash throughput works yay

--- lb/dash.js
+++ lb/dash.js
@@ -3,6 +3,13 @@ var example = 'dynamic-update',
   theme = 'default';
 
 var chart; // global
+var seriesMapper = {};
+
+Highcharts.setOptions({
+  global: {
+    useUTC: false
+  }
+});
 
 function requestData() {
   $.ajax({
@@ -10,10 +17,9 @@ function requestData() {
     success: function(point) {
       console.log(point)
       var jason = JSON.parse(point);
-      console.log(jason)
 
-      var series = chart.series[0],
-        shift = series.data.length > 20;
+      //var series = chart.series[0],
+      //shift = series.data.length > 20;
 
       if (!jason["stats"] || jason["stats"].length == 0) {
         // XXX (reed): using server timestamps for real data this can drift easily
@@ -27,22 +33,28 @@ function requestData() {
       }
 
       for (var i = 0; i < jason["stats"].length; i++) {
-        // set up the updating of the chart each second
-
-        //var node = jason["stats"]
-        //var series = chart.get(node)
-        //if (!series) {
-          //chart.addSeries({name: node, data: []})
-          //series = chart.get(node) // XXX (reed): meh
-        //}
-
-        //shift = series.data.length > 20;
-
         stat = jason["stats"][i];
+        var node = stat["node"];
+
+        console.log("before", seriesMapper[node])
+        if (seriesMapper[node] == null) {
+          console.log("yodawg")
+          chart.addSeries({name: node, data: []})
+          seriesMapper[node] = chart.series.length - 1
+          chart.redraw();
+        }
+
+        console.log("done", seriesMapper[node])
+        series = chart.series[seriesMapper[node]]
+        //series = chart.series[0]
+        shift = series.data.length > 20;
+
+
         timestamp = Date.parse(stat["timestamp"]);
-         series.addPoint([timestamp, stat["tp"]], true, shift);
+        console.log(series.data.length, timestamp, stat["tp"])
+        series.addPoint([timestamp, stat["tp"]], true, shift);
         //series.addPoint({
-          //name: stat["node"],
+          //name: node,
           //data: {x: timestamp, y: stat["tp"]}
         //}, true, shift);
       }
@@ -59,30 +71,32 @@ $(document).ready(function() {
   chart = new Highcharts.Chart({
     chart: {
       renderTo: 'container',
-      defaultSeriesType: 'spline',
       events: {
         load: requestData
       }
     },
+    rangeSelector: {
+      buttons: [{
+        count: 1,
+        type: 'minute',
+        text: '1M'
+      }, {
+        count: 5,
+        type: 'minute',
+        text: '5M'
+      }, {
+        type: 'all',
+        text: 'All'
+      }],
+      //inputEnabled: false,
+      selected: 0
+    },
     title: {
-      text: 'Live random data'
+      text: 'lb data'
     },
-    xAxis: {
-      type: 'datetime',
-      tickPixelInterval: 150,
-      maxZoom: 20 * 1000
+    exporting: {
+      enabled: false
     },
-    yAxis: {
-      minPadding: 0.2,
-      maxPadding: 0.2,
-      title: {
-        text: 'Value',
-        margin: 80
-      }
-    },
-    series: [{
-      name: 'Random data',
-      data: []
-    }]
+    series: []
   });
 });
