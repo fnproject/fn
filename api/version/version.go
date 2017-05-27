71b66091df1b8d36e3d7275a5dd0e1035d0d8f1f
Travis Reeder <treeder@gmail.com>
2017-05-27 09:47:08 -0700

    functions: 0.3.5 release [skip ci]

--- api/version/version.go
+++ api/version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // Version of Functions
-var Version = "0.3.4"
+var Version = "0.3.5"
--- fn/install.sh
+++ fn/install.sh
@@ -3,7 +3,7 @@ set -e
 
 # Install script to install fn
 
-release="0.3.4"
+release="0.3.5"
 
 command_exists() {
   command -v "$@" > /dev/null 2>&1
