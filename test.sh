42b80c19bab6b482b8788e61d67d05fbd67ba8e4
Denis Makogon <denis.makogon@oracle.com>
2017-05-31 01:12:09 -0700

    Propogating ${FN} outside of `test.sh` script

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -ex
 go test -v $(go list ./... | grep -v vendor | grep -v examples | grep -v tool | grep -v fn)
 cd fn && make build && make test
 # TODO: should we install fn here to use throughout?
-FN="$(pwd)/fn"
+export FN="$(pwd)/fn"
 cd ..
 
 # TODO: Test a bunch of the examples using fn test when ready
