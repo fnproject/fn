4e511fd66c73f97f11da35c02929d214980cc77d
Matthew Gilliard <matthew.gilliard@oracle.com>
2017-06-02 16:20:25 +0100

    Adds java-maven runtime

--- fn/langs/base.go
+++ fn/langs/base.go
@@ -31,6 +31,8 @@ func GetLangHelper(lang string) LangHelper {
 		return &LambdaNodeHelper{}
 	case "java":
 		return &JavaLangHelper{}
+	case "java-maven":
+		return &JavaMavenLangHelper{}
 	}
 	return nil
 }
