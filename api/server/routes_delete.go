67719d691d596688fc606d189469ecece5e5e690
Denis Makogon <lildee1991@gmail.com>
2017-06-30 14:09:00 +0300

    Check if route exist before attempting to delete it
    xFixes: #76

--- api/server/routes_delete.go
+++ api/server/routes_delete.go
@@ -15,6 +15,11 @@ func (s *Server) handleRouteDelete(c *gin.Context) {
 	appName := c.MustGet(api.AppName).(string)
 	routePath := path.Clean(c.MustGet(api.Path).(string))
 
+	if _, err := s.Datastore.GetRoute(ctx, appName, routePath); err != nil {
+		handleErrorResponse(c, err)
+		return
+	}
+	
 	if err := s.Datastore.RemoveRoute(ctx, appName, routePath); err != nil {
 		handleErrorResponse(c, err)
 		return
