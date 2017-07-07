7343df70def0e01e14f29ca8820df7e3f33ca39d
James Jeffrey <james.jeffrey@oracle.com>
2017-07-07 10:35:36 -0700

    Add example of using vendor for golang

---
+++ examples/tutorial/hello/go/usingdeps/func.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Person struct {
+	Name string
+}
+
+func main() {
+	p := &Person{Name: "World"}
+	json.NewDecoder(os.Stdin).Decode(p)
+	logrus.Printf("Hello %v!\n", p.Name)
+
+	logrus.Println("---> stderr goes to the server logs.")
+	logrus.Println("---> LINE 2")
+	logrus.Println("---> LINE 3 with a break right here\nand LINE 4")
+	logrus.Println("---> LINE 5 with a double line break\n ")
+	logrus.Println("---> LINE 6")
+}
