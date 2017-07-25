5b41fe2dc7f9e2d853fa501e260b295235415891
Denis Makogon <denis.makogon@oracle.com>
2017-07-25 10:29:20 -0700

    Improving API tests

--- test/fn-api-tests/calls_test.go
+++ test/fn-api-tests/calls_test.go
@@ -11,9 +11,10 @@ import (
 )
 
 func TestCalls(t *testing.T) {
-	s := SetupDefaultSuite()
 
 	t.Run("list-calls-for-missing-app", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
 		cfg := &call.GetAppsAppCallsRouteParams{
 			App:     s.AppName,
 			Route:   s.RoutePath,
@@ -21,19 +22,15 @@ func TestCalls(t *testing.T) {
 		}
 		_, err := s.Client.Call.GetAppsAppCallsRoute(cfg)
 		if err == nil {
-			t.Fatalf("Must fail with missing app error, but got %s", err)
+			t.Errorf("Must fail with missing app error, but got %s", err)
 		}
 	})
-
-	CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
-
-	u := url.URL{
-		Scheme: "http",
-		Host:   Host(),
-	}
-	u.Path = path.Join(u.Path, "r", s.AppName, s.RoutePath)
 
 	t.Run("list-calls-for-missing-route", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+
 		cfg := &call.GetAppsAppCallsRouteParams{
 			App:     s.AppName,
 			Route:   s.RoutePath,
@@ -41,14 +38,19 @@ func TestCalls(t *testing.T) {
 		}
 		_, err := s.Client.Call.GetAppsAppCallsRoute(cfg)
 		if err == nil {
-			t.Fatalf("Must fail with missing route error, but got %s", err)
+			t.Errorf("Must fail with missing route error, but got %s", err)
 		}
+
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
-	CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
-		s.RouteConfig, s.RouteHeaders)
-
 	t.Run("get-dummy-call", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
+
 		cfg := &call.GetCallsCallParams{
 			Call:    "dummy",
 			Context: s.Context,
@@ -56,12 +58,25 @@ func TestCalls(t *testing.T) {
 		cfg.WithTimeout(time.Second * 60)
 		_, err := s.Client.Call.GetCallsCall(cfg)
 		if err == nil {
-			t.Fatal("Must fail because `dummy` call does not exist.")
+			t.Error("Must fail because `dummy` call does not exist.")
 		}
-		t.Logf("Test `%v` passed.", t.Name())
+
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("get-real-call", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
+
+		u := url.URL{
+			Scheme: "http",
+			Host:   Host(),
+		}
+		u.Path = path.Join(u.Path, "r", s.AppName, s.RoutePath)
 
 		callID := CallAsync(t, u, &bytes.Buffer{})
 		time.Sleep(time.Second * 5)
@@ -75,13 +90,29 @@ func TestCalls(t *testing.T) {
 			switch err.(type) {
 			case *call.GetCallsCallNotFound:
 				msg := err.(*call.GetCallsCallNotFound).Payload.Error.Message
-				t.Fatalf("Unexpected error occurred: %v.", msg)
+				t.Errorf("Unexpected error occurred: %v.", msg)
 			}
 		}
-		t.Logf("Test `%v` passed.", t.Name())
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("list-calls", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
+
+		u := url.URL{
+			Scheme: "http",
+			Host:   Host(),
+		}
+		u.Path = path.Join(u.Path, "r", s.AppName, s.RoutePath)
+
+		CallAsync(t, u, &bytes.Buffer{})
+		time.Sleep(time.Second * 8)
+
 		cfg := &call.GetAppsAppCallsRouteParams{
 			App:     s.AppName,
 			Route:   s.RoutePath,
@@ -89,18 +120,18 @@ func TestCalls(t *testing.T) {
 		}
 		calls, err := s.Client.Call.GetAppsAppCallsRoute(cfg)
 		if err != nil {
-			t.Fatalf("Unexpected error: %s", err)
+			t.Errorf("Unexpected error: %s", err)
 		}
 		if len(calls.Payload.Calls) == 0 {
-			t.Fatalf("Must fail. There should be at least one call to `%v` route.", s.RoutePath)
+			t.Errorf("Must fail. There should be at least one call to `%v` route.", s.RoutePath)
 		}
 		for _, c := range calls.Payload.Calls {
 			if c.Path != s.RoutePath {
-				t.Fatalf("Call path mismatch.\n\tExpected: %v\n\tActual: %v", c.Path, s.RoutePath)
+				t.Errorf("Call path mismatch.\n\tExpected: %v\n\tActual: %v", c.Path, s.RoutePath)
 			}
 		}
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
-	DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
-	DeleteApp(t, s.Context, s.Client, s.AppName)
 }
--- test/fn-api-tests/routes_test.go
+++ test/fn-api-tests/routes_test.go
@@ -1,41 +1,63 @@
 package tests
 
+//
 import (
 	"testing"
 
 	"github.com/funcy/functions_go/models"
+	"gitlab-odx.oracle.com/odx/functions/api/id"
 )
 
 func TestRoutes(t *testing.T) {
-	s := SetupDefaultSuite()
-
 	newRouteType := "sync"
-	newRoutePath := "/new-hello"
-
-	CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+	newRoutePath := id.New().String()
 
 	t.Run("create-route", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
 		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
 			s.RouteConfig, s.RouteHeaders)
-		t.Logf("Test `%v` passed.", t.Name())
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("list-and-find-route", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
 		if !assertContainsRoute(ListRoutes(t, s.Context, s.Client, s.AppName), s.RoutePath) {
-			t.Fatalf("Unable to find corresponding route `%v` in list", s.RoutePath)
+			t.Errorf("Unable to find corresponding route `%v` in list", s.RoutePath)
 		}
-		t.Logf("Test `%v` passed.", t.Name())
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("can-get-corresponding-route", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
+
 		rObjects := []*models.Route{GetRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)}
 		if !assertContainsRoute(rObjects, s.RoutePath) {
-			t.Fatalf("Unable to find corresponding route `%v` in list", s.RoutePath)
+			t.Errorf("Unable to find corresponding route `%v` in list", s.RoutePath)
 		}
-		t.Logf("Test `%v` passed.", t.Name())
+
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("can-update-route-info", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
+
 		routeResp, err := UpdateRoute(
 			t, s.Context, s.Client,
 			s.AppName, s.RoutePath,
@@ -45,39 +67,66 @@ func TestRoutes(t *testing.T) {
 		CheckRouteResponseError(t, err)
 		assertRouteFields(t, routeResp.Payload.Route, s.RoutePath, s.Image, newRouteType)
 
-		t.Logf("Test `%v` passed.", t.Name())
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("fail-to-update-route-path", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
+
 		_, err := UpdateRoute(
 			t, s.Context, s.Client,
 			s.AppName, s.RoutePath,
 			s.Image, s.RouteType, s.Format,
 			s.Memory, s.RouteConfig, s.RouteHeaders, newRoutePath)
 		if err == nil {
-			t.Fatalf("Route path suppose to be immutable, but it's not.")
+			t.Errorf("Route path suppose to be immutable, but it's not.")
 		}
-		t.Logf("Test `%v` passed.", t.Name())
+
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("create-route-duplicate", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
+
 		_, err := createRoute(s.Context, s.Client, s.AppName, s.Image, s.RoutePath, newRouteType, s.RouteConfig, s.RouteHeaders)
 		if err == nil {
-			t.Fatalf("Route duplicate error should appear, but it didn't")
+			t.Errorf("Route duplicate error should appear, but it didn't")
 		}
+
+		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("can-delete-route", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+		CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
+			s.RouteConfig, s.RouteHeaders)
+
 		DeleteRoute(t, s.Context, s.Client, s.AppName, s.RoutePath)
-		t.Logf("Test `%v` passed.", t.Name())
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
 
 	t.Run("fail-to-delete-missing-route", func(t *testing.T) {
+		t.Parallel()
+		s := SetupDefaultSuite()
+		CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
+
 		_, err := deleteRoute(s.Context, s.Client, s.AppName, "dummy-route")
 		if err == nil {
-			t.Fatal("Delete from missing route must fail.")
+			t.Error("Delete from missing route must fail.")
 		}
+		DeleteApp(t, s.Context, s.Client, s.AppName)
 	})
-
-	DeleteApp(t, s.Context, s.Client, s.AppName)
 }
