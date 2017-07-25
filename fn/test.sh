1adb800ce1e92d5c6ab5926a7e93fbf11b968341
Travis Reeder <treeder@gmail.com>
2017-07-25 09:14:35 -0700

    Changed port and testing to see that build will fail on fn commands.

--- fn/test.sh
+++ fn/test.sh
@@ -8,17 +8,20 @@ $fn --version
 rm -rf tmp
 mkdir tmp
 cd tmp
-$fn init --runtime go $DOCKER_USERNAME/fn-test-go
+funcname="tn-test-go"
+$fn init --runtime go $DOCKER_USERNAME/$funcname
 $fn test
 
+someport=50080
 docker rm --force functions || true # just in case
-docker run --name functions -d -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 funcy/functions
+docker run --name functions -d -v /var/run/docker.sock:/var/run/docker.sock -p $someport:8080 funcy/functions
 
 sleep 10
+export API_URL="http://localhost:$someport"
 $fn apps l
 $fn apps create myapp
 $fn deploy myapp
-$fn call myapp /fn-test-go3
+$fn call myapp $DOCKER_USERNAME/$funcname
 
 docker rm --force functions
 
