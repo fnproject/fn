342c66396db0e190369d51d9082412c1e53faf46
Travis Reeder <treeder@gmail.com>
2017-07-26 15:52:34 -0700

    testing cli release only

--- cli/release.sh
+++ cli/release.sh
@@ -39,32 +39,15 @@ git push
 git push origin $tag
 
 # For GitHub
-# url='https://api.github.com/repos/treeder/functions/releases'
-# output=$(curl -s -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY -d "{\"tag_name\": \"$version\", \"name\": \"$version\"}" $url)
-# upload_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload_url"]' | sed -E "s/\{.*//")
-# html_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["html_url"]')
-# curl --data-binary "@fn_linux"  -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=fn_linux >/dev/null
-# curl --data-binary "@fn_mac"    -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=fn_mac >/dev/null
-# curl --data-binary "@fn.exe"    -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=fn.exe >/dev/null
-
-# For GitLab
-# 1) Upload files: https://docs.gitlab.com/ee/api/projects.html#upload-a-file
-upload_url='https://gitlab-odx.oracle.com/api/v3/projects/9/uploads'
-output=$(curl --request POST --form "file=@fn_linux" --header "PRIVATE-TOKEN: $GITLAB_TOKEN" $upload_url)
-linux_markdown=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["markdown"]')
-output=$(curl --request POST --form "file=@fn_mac" --header "PRIVATE-TOKEN: $GITLAB_TOKEN" $upload_url)
-mac_markdown=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["markdown"]')
-output=$(curl --request POST --form "file=@fn.exe" --header "PRIVATE-TOKEN: $GITLAB_TOKEN" $upload_url)
-win_markdown=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["markdown"]')
-output=$(curl --request POST --form "file=@fn_alpine" --header "PRIVATE-TOKEN: $GITLAB_TOKEN" $upload_url)
-alpine_markdown=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["markdown"]')
-
-# 2) Create a release: https://docs.gitlab.com/ee/api/tags.html#create-a-new-release
-release_url="https://gitlab-odx.oracle.com/api/v3/projects/9/repository/tags/$tag/release"
-release_desc="Awesome release, much Wow\n\nfn for Linux: $linux_markdown \n\nfn for Mac: $mac_markdown \n\nfn for Windows: $win_markdown \n\nfn for Alpine: $alpine_markdown"
-curl --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" -H "Content-Type: application/json" -d "{\"tag_name\": \"$tag\", \"description\": \"$release_desc\"}" $release_url
+url='https://api.github.com/repos/fnproject/fn/releases'
+output=$(curl -s -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY -d "{\"tag_name\": \"$version\", \"name\": \"$version\"}" $url)
+upload_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload_url"]' | sed -E "s/\{.*//")
+html_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["html_url"]')
+curl --data-binary "@fn_linux"  -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=fn_linux >/dev/null
+curl --data-binary "@fn_mac"    -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=fn_mac >/dev/null
+curl --data-binary "@fn.exe"    -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=fn.exe >/dev/null
 
 # TODO: Add the download URLS to install.sh. Maybe we should make a template to generate install.sh
 # TODO: Download URL's are in the output vars above under "url". Eg: "url":"/uploads/9a1848c5ebf2b83f8b055ac0e50e5232/fn.exe"
-# sed "s/release=.*/release=\"$version\"/g" fn/install.sh > fn/install.sh.tmp
-# mv fn/install.sh.tmp fn/install.sh
+sed "s/release=.*/release=\"$version\"/g" fn/install.sh > fn/install.sh.tmp
+mv cli/install.sh.tmp cli/install.sh
--- release.sh
+++ release.sh
@@ -22,22 +22,25 @@ if [ -z $(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file) ]; then
   echo "did not find semantic version in $version_file"
   exit 1
 fi
-perl -i -pe 's/\d+\.\d+\.\K(\d+)/$1+1/e' $version_file
+# TODO: ADD BACK!!
+# perl -i -pe 's/\d+\.\d+\.\K(\d+)/$1+1/e' $version_file
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
 
-make docker-build
+# TODO: ADD BACK!!
+# make docker-build
 
-git add -u
-git commit -m "$service: $version release [skip ci]"
-git tag -f -a "$version" -m "version $version"
-git push
-git push origin $version
+# git add -u
+# git commit -m "$service: $version release [skip ci]"
+# git tag -f -a "$version" -m "version $version"
+# git push
+# git push origin $version
 
 # Finally tag and push docker images
-docker tag $user/$service:$tag $user/$service:$version
-docker push $user/$service:$version
-docker push $user/$service:$tag
+# TODO: ADD BACK!!
+# docker tag $user/$service:$tag $user/$service:$version
+# docker push $user/$service:$version
+# docker push $user/$service:$tag
 
 cd cli
 ./release.sh $version
