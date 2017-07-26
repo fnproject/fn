3c83e186a5102123c514afb78e5d975f276d0293
Travis Reeder <treeder@gmail.com>
2017-07-26 13:32:01 -0700

    circle

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+make build
+
 docker rm -fv func-postgres-test || echo No prev test db container
 docker run --name func-postgres-test -p 15432:5432 -d postgres
 docker rm -fv func-mysql-test || echo No prev mysql test db container
