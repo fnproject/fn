ecd4cce24a66e53520558209be00820a949831f4
Travis Reeder <treeder@gmail.com>
2017-07-26 17:00:25 -0700

    Added back in commented out lines.

--- release.sh
+++ release.sh
@@ -22,25 +22,22 @@ if [ -z $(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file) ]; then
   echo "did not find semantic version in $version_file"
   exit 1
 fi
-# TODO: ADD BACK!!
-# perl -i -pe 's/\d+\.\d+\.\K(\d+)/$1+1/e' $version_file
+perl -i -pe 's/\d+\.\d+\.\K(\d+)/$1+1/e' $version_file
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
 
-# TODO: ADD BACK!!
-# make docker-build
+make docker-build
 
-# git add -u
-# git commit -m "$service: $version release [skip ci]"
-# git tag -f -a "$version" -m "version $version"
-# git push
-# git push origin $version
+git add -u
+git commit -m "$service: $version release [skip ci]"
+git tag -f -a "$version" -m "version $version"
+git push
+git push origin $version
 
 # Finally tag and push docker images
-# TODO: ADD BACK!!
-# docker tag $user/$service:$tag $user/$service:$version
-# docker push $user/$service:$version
-# docker push $user/$service:$tag
+docker tag $user/$service:$tag $user/$service:$version
+docker push $user/$service:$version
+docker push $user/$service:$tag
 
 cd cli
 ./release.sh $version
