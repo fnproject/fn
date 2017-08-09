bac669350776d4385714b7f06660a238fe0d6abd
Denis Makogon <lildee1991@gmail.com>
2017-08-10 00:39:48 +0300

    Adding "memory" CLI flag to fn run

--- cli/run.go
+++ cli/run.go
@@ -56,6 +56,11 @@ func runflags() []cli.Flag {
 			Name:  "runs",
 			Usage: "for hot functions only, will call the function `runs` times in a row.",
 		},
+		cli.Uint64Flag{
+			Name:  "memory",
+			Usage: "RAM to allocate for function, Units: MB",
+			Value: uint64(128),
+		},
 	}
 }
 
@@ -80,6 +85,7 @@ func (r *runCmd) run(c *cli.Context) error {
 		ff = &funcfile{
 			Name: image,
 		}
+		ff.Memory = c.Uint64("memory")
 	}
 
 	return runff(ff, stdin(), os.Stdout, os.Stderr, c.String("method"), c.StringSlice("e"), c.StringSlice("link"), c.String("format"), c.Int("runs"))
@@ -87,7 +93,7 @@ func (r *runCmd) run(c *cli.Context) error {
 
 // TODO: share all this stuff with the Docker driver in server or better yet, actually use the Docker driver
 func runff(ff *funcfile, stdin io.Reader, stdout, stderr io.Writer, method string, envVars []string, links []string, format string, runs int) error {
-	sh := []string{"docker", "run", "--rm", "-i"}
+	sh := []string{"docker", "run", "--rm", "-i", fmt.Sprintf("--memory=%dm", ff.Memory)}
 
 	var err error
 	var env []string    // env for the shelled out docker run command
@@ -109,6 +115,7 @@ func runff(ff *funcfile, stdin io.Reader, stdout, stderr io.Writer, method strin
 	runEnv = append(runEnv, kvEq("APP_NAME", "myapp"))
 	runEnv = append(runEnv, kvEq("ROUTE", "/hello")) // TODO: should we change this to PATH ?
 	runEnv = append(runEnv, kvEq("FN_FORMAT", format))
+	runEnv = append(runEnv, kvEq("MEMORY_MB", fmt.Sprintf("%d", ff.Memory)))
 
 	// add user defined envs
 	runEnv = append(runEnv, envVars...)
