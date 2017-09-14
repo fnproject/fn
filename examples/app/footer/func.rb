5476665ea92458beafafa09de72fb897baa439b4
Travis Reeder <treeder@gmail.com>
2017-09-14 16:30:30 -0700

    Quick link fix.

--- examples/app/footer/func.rb
+++ examples/app/footer/func.rb
@@ -1,6 +1,8 @@
 puts %{
     <div style="margin-top: 20px; border-top: 1px solid gray;">
-        <div><a href="/r/helloapp/ruby">Ruby</a></div><div><a href="/r/helloapp/node">Node</a></div><div><a href="/r/helloapp/python">Python</a></div>
+        <div><a href="/r/#{ENV['FN_APP_NAME']}/ruby">Ruby</a></div>
+        <div><a href="/r/#{ENV['FN_APP_NAME']}/node">Node</a></div>
+        <div><a href="/r/#{ENV['FN_APP_NAME']}/python">Python</a></div>
     </div>
   </body>
 </html>
