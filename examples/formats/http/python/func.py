5dca94689a616805462f7159fa34d940bbf59312
Denis Makogon <lildee1991@gmail.com>
2017-10-01 12:09:07 +0300

    Updating Python hot HTTP sample
     - using latest hotfn 0.0.5 release (made life lot easier)

--- examples/formats/http/python/func.py
+++ examples/formats/http/python/func.py
@@ -1,42 +1,24 @@
-import asyncio
-import sys
-import uvloop
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-from hotfn.http import request
-from hotfn.http import response
+from hotfn.http import main
 
 
-class MyProtocol(asyncio.Protocol):
-
-    def connection_made(self, transport):
-        print('pipe opened', file=sys.stderr, flush=True)
-        super(MyProtocol, self).connection_made(transport=transport)
-
-    def data_received(self, data):
-        print('received: {!r}'.format(data), file=sys.stderr, flush=True)
-        data = data.decode()
-        req = request.RawRequest(data)
-        (method, url, dict_params,
-         headers, http_version, req_data) = req.parse_raw_request()
-
-        rs = response.RawResponse(
-            http_version, 200, "OK",
-            response_data=req_data)
-        print(rs.dump(), file=sys.stdout, flush=True)
-        super(MyProtocol, self).data_received(data)
-
-    def connection_lost(self, exc):
-        print('pipe closed', file=sys.stderr, flush=True)
-        super(MyProtocol, self).connection_lost(exc)
+@main.coerce_input_to_content_type
+def app(s):
+    return s
 
 
 if __name__ == "__main__":
-    with open("/dev/stdin", "rb", buffering=0) as stdin:
-        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-        loop = asyncio.get_event_loop()
-        try:
-            stdin_pipe_reader = loop.connect_read_pipe(MyProtocol, stdin)
-            loop.run_until_complete(stdin_pipe_reader)
-            loop.run_forever()
-        finally:
-            loop.close()
+    main.main(app)
