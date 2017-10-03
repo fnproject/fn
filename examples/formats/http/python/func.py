f3f5284f6a3df8d907e8b52e116630102d8f0534
Denis Makogon <lildee1991@gmail.com>
2017-10-04 00:32:25 +0300

    Updating to latest 0.0.6 release
     What's new?
     - new request data signature: context, data, loop
     - supports event loop (very useful for request body handeler of coroutine type)

--- examples/formats/http/python/func.py
+++ examples/formats/http/python/func.py
@@ -12,13 +12,24 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from hotfn.http import main
+from hotfn.http import worker
 
 
-@main.coerce_input_to_content_type
-def app(s):
-    return s
+@worker.coerce_input_to_content_type
+def app(context, data=None, loop=None):
+    """
+    User's request body handler
+    :param context: request context
+    :type context: hotfn.http.request.RequestContext
+    :param data: request body, it's type depends on request Content-Type header
+    :type data: object
+    :param loop: asyncio event loop
+    :type loop asyncio.AbstractEventLoop
+    :return: echo of data
+    :rtype: object
+    """
+    return data
 
 
 if __name__ == "__main__":
-    main.main(app)
+    worker.run(app)
