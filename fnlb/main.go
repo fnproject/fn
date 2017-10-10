881160aae8a3eb096a1cfb6ec7139dae668d8c0e
CI <ci@fnproject.com>
2017-10-10 08:48:10 +0000

    fnlb: 0.0.114 release [skip ci]

--- fnlb/main.go
+++ fnlb/main.go
@@ -17,12 +17,12 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const VERSION = "0.0.113"
+const VERSION = "0.0.114"
 
 func main() {
 	// XXX (reed): normalize
 	fnodes := flag.String("nodes", "", "comma separated list of functions nodes")
-	minAPIVersion := flag.String("min-api-version", "0.0.80", "minimal node API to accept")
+	minAPIVersion := flag.String("min-api-version", "0.0.81", "minimal node API to accept")
 
 	var conf lb.Config
 	flag.StringVar(&conf.DBurl, "db", "sqlite3://:memory:", "backend to store nodes, default to in memory")
