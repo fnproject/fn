e1f15cc6528ba55652d67940669a2421e8d8e0dd
Tolga Ceylan <tolga.ceylan@gmail.com>
2017-11-20 22:17:27 -0800

    fn: fnlb: fix tests if behind http proxy
    *) localhost to avoid http proxy interception

--- fnlb/lb/allgrouper_test.go
+++ fnlb/lb/allgrouper_test.go
@@ -118,7 +118,7 @@ func teardownServer(t *testing.T, server *http.Server) {
 // spin up backend servers
 func initializeAPIServer(t *testing.T, grouper Grouper) (*http.Server, string, error) {
 
-	listener, err := net.Listen("tcp", ":0")
+	listener, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		return nil, "", err
 	}
@@ -145,7 +145,7 @@ func initializeTestServers(t *testing.T, numOfServers uint64) ([]*testServer, er
 
 	for i := uint64(0); i < numOfServers; i++ {
 
-		listener, err := net.Listen("tcp", ":0")
+		listener, err := net.Listen("tcp", "localhost:0")
 		if err != nil {
 			return list, err
 		}
