13fa2cc48fea19c1a78c792ffc41da41ca9c7f36
Denis Makogon <lildee1991@gmail.com>
2017-12-22 00:39:27 +0200

    Make Logs API backward compatible

--- api/server/call_logs.go
+++ api/server/call_logs.go
@@ -5,8 +5,15 @@ import (
 	"net/http"
 
 	"github.com/fnproject/fn/api"
+	"github.com/fnproject/fn/api/models"
 	"github.com/gin-gonic/gin"
 )
+
+// note: for backward compatibility, will go away later
+type callLogResponse struct {
+	Message string          `json:"message"`
+	Log     *models.CallLog `json:"log"`
+}
 
 func (s *Server) handleCallLogGet(c *gin.Context) {
 	ctx := c.Request.Context()
@@ -23,5 +30,16 @@ func (s *Server) handleCallLogGet(c *gin.Context) {
 	var b bytes.Buffer
 	b.ReadFrom(logReader)
 
-	c.String(http.StatusOK, b.String())
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		callObj := models.CallLog{
+			CallID:  callID,
+			AppName: appName,
+			Log:     b.String(),
+		}
+
+		c.JSON(http.StatusOK, callLogResponse{"Successfully loaded log", &callObj})
+	default:
+		c.String(http.StatusOK, b.String())
+	}
 }
