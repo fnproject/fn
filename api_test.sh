c2f06c3528620ee75eda80f768d8a609cb7098b8
Denis Makogon <lildee1991@gmail.com>
2017-12-23 23:49:02 +0200

    Build API tests binary once
     This patch lets CI to build API tests binary once and reuse that whenever it needs it

--- api_test.sh
+++ api_test.sh
@@ -24,17 +24,16 @@ case "$1" in
     "sqlite3" )
     rm -fr /tmp/fn_integration_tests.db
     touch /tmp/fn_integration_tests.db
-    FN_DB_URL="sqlite3:///tmp/fn_integration_tests.db"
+    export FN_DB_URL="sqlite3:///tmp/fn_integration_tests.db"
     ;;
 
     "mysql" )
     DB_CONTAINER="func-mysql-test"
     docker rm -fv ${DB_CONTAINER} || echo No prev mysql test db container
     docker run --name ${DB_CONTAINER} -p 3306:3306 -e MYSQL_DATABASE=funcs -e MYSQL_ROOT_PASSWORD=root -d mysql
-    sleep 5
     MYSQL_HOST=`host ${DB_CONTAINER}`
     MYSQL_PORT=3306
-    FN_DB_URL="mysql://root:root@tcp(${MYSQL_HOST}:${MYSQL_PORT})/funcs"
+    export FN_DB_URL="mysql://root:root@tcp(${MYSQL_HOST}:${MYSQL_PORT})/funcs"
 
     ;;
 
@@ -42,12 +41,18 @@ case "$1" in
     DB_CONTAINER="func-postgres-test"
     docker rm -fv ${DB_CONTAINER} || echo No prev test db container
     docker run --name ${DB_CONTAINER} -e "POSTGRES_DB=funcs" -e "POSTGRES_PASSWORD=root"  -p 5432:5432 -d postgres
-    sleep 5
     POSTGRES_HOST=`host ${DB_CONTAINER}`
     POSTGRES_PORT=5432
-    FN_DB_URL="postgres://postgres:root@${POSTGRES_HOST}:${POSTGRES_PORT}/funcs?sslmode=disable"
+    export FN_DB_URL="postgres://postgres:root@${POSTGRES_HOST}:${POSTGRES_PORT}/funcs?sslmode=disable"
 
     ;;
 esac
 
-cd test/fn-api-tests && FN_API_URL="http://localhost:8080"  FN_DB_URL=${FN_DB_URL} go test -v  -parallel ${2:-1} ./...; cd ../../
+test test/fn-api-tests/fn-api-tests.test
+status=`echo $?`
+rebuild="${3:-0}"
+if [[ $status -ne 0 ]] || [[ $rebuild -ne 0 ]] ; then
+    go test -i -a
+fi
+
+./test/fn-api-tests/fn-api-tests.test -test.v  -test.parallel ${2:-1} ./...; cd ../../
