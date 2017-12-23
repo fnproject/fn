3c0edd25665201d894df08da9dde0b24e1ebb9d3
Denis Makogon <lildee1991@gmail.com>
2017-12-23 22:20:45 +0200

    Remove dup test case

--- test/fn-api-tests/calls_test.go
+++ test/fn-api-tests/calls_test.go
@@ -45,35 +45,6 @@ func TestCallsDummy(t *testing.T) {
 	DeleteApp(t, s.Context, s.Client, s.AppName)
 }
 
-func TestGetCallsSuccess(t *testing.T) {
-	t.Parallel()
-	s := SetupDefaultSuite()
-	CreateApp(t, s.Context, s.Client, s.AppName, map[string]string{})
-	CreateRoute(t, s.Context, s.Client, s.AppName, s.RoutePath, s.Image, s.RouteType,
-		s.Format, s.Timeout, s.IdleTimeout, s.RouteConfig, s.RouteHeaders)
-
-	u := url.URL{
-		Scheme: "http",
-		Host:   Host(),
-	}
-	u.Path = path.Join(u.Path, "r", s.AppName, s.RoutePath)
-
-	cfg := &call.GetAppsAppCallsParams{
-		App:  s.AppName,
-		Path: &s.RoutePath,
-	}
-	retryErr := APICallWithRetry(t, 10, time.Second*2, func() (err error) {
-		_, err = s.Client.Call.GetAppsAppCalls(cfg)
-		return err
-	})
-
-	if retryErr != nil {
-		t.Error(retryErr.Error())
-	}
-
-	DeleteApp(t, s.Context, s.Client, s.AppName)
-}
-
 func TestListCallsSuccess(t *testing.T) {
 	t.Parallel()
 	s := SetupDefaultSuite()
