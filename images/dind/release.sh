eef1025e9395597d99251468bbb9bf2333599101
Travis Reeder <treeder@gmail.com>
2018-01-09 15:33:26 -0800

    Better versioning for dind image. (#666)
    * Better versioning for dind image.
    
    * Updated dind image used in main build

--- images/dind/release.sh
+++ images/dind/release.sh
@@ -1,12 +1,19 @@
-set -e
+set -ex
 
-./build.sh
+# ./build.sh
 
 # Match version with Docker version
 version=$(docker run --rm -v "$PWD":/app treeder/bump  --extract --input "`docker -v`")
 echo "Version: $version"
+M=$(docker run --rm treeder/bump --format M --input "$version")
+Mm=$(docker run --rm treeder/bump --format M.m --input "$version")
 
 docker tag fnproject/dind:latest fnproject/dind:$version
-
-docker push fnproject/dind:latest
+# be nice to have bump image do all of this tagging and pushing too (mount docker sock and do it all)
+docker tag fnproject/dind:$version fnproject/dind:$Mm
+docker tag fnproject/dind:$version fnproject/dind:$M
+ 
 docker push fnproject/dind:$version
+docker push fnproject/dind:$Mm
+docker push fnproject/dind:$M
+docker push fnproject/dind:latest
