ce457735d469ffccf82adffc1e664fe4ffdb3b75
jan grant <jan.grant@oracle.com>
2018-01-15 12:18:50 +0000

    Fix for MTU problems in some k8s environments
    * NOTE: the fnproject/dind release will need recutting and the
      top-level Dockerfile updated to refer to it for this to be
      complete.
    
    In many k8s environments the host docker uses an overlay network
    which'll take bytes away from the effective MTU of outer
    containers; eg, vxlan needs 50 bytes, often leaving a 1450 MTU
    on the container running dind and fn-api.
    
    In such an arrangement, packets exceeding the smaller MTU may be
    invisibly dropped as they travel across the dind's docker0
    bridge. This mostly surfaces as a failure of functions to be able
    to reliably talk to external services. (Note, the failure may be
    intermittent depending on the profile of the resulting TCP
    communication.)
    
    A robust fix for this is to intercept the startup of the dind
    dockerd and ensure that /etc/docker/daemon.json (currently
    absent) contains the following setting:
    
    {
      "mtu": 1450
    }
    
    (or whatever the MTU on the external interface may be). This
    should be autosized so the container works in a variety of
    deployments.
    
    The problem does not arise when using an embedded
    /var/run/docker.sock - or when running with dind on a host that
    can supply 1500-byte MTUs to containers on the 'host' docker.

--- images/dind/preentry.sh
+++ images/dind/preentry.sh
@@ -6,6 +6,15 @@ if [ $fsdriver == "overlay" ]; then
   fsdriver="overlay2"
 fi
 
+mkdir -p /etc/docker
+/bin/cat > /etc/docker/daemon.json <<EOF
+{
+  "mtu": $(ip link show dev $(ip route |
+                              awk '$1 == "default" { print $NF }') |
+           awk '{for (i = 1; i <= NF; i++) if ($i == "mtu") print $(i+1)}')
+}
+EOF
+
 dockerd-entrypoint.sh --storage-driver=$fsdriver &
 
 # give docker a few seconds
