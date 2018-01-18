d9c1329e8cc91edf53202c51679d3e80839dcde6
Michael Williams <michael.w.williams@oracle.com>
2018-01-18 09:04:49 -0700

    Adding Hello World tutorial for Java

---
+++ examples/tutorial/hello/java/src/test/java/com/example/fn/HelloFunctionTest.java
@@ -0,0 +1,22 @@
+package com.example.fn;
+
+import com.fnproject.fn.testing.*;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class HelloFunctionTest {
+
+    @Rule
+    public final FnTestingRule testing = FnTestingRule.createDefault();
+
+    @Test
+    public void shouldReturnGreeting() {
+        testing.givenEvent().enqueue();
+        testing.thenRun(HelloFunction.class, "handleRequest");
+
+        FnResult result = testing.getOnlyResult();
+        assertEquals("Hello, world!", result.getBodyAsString());
+    }
+
+}
