4a58cde5a30a93e3f1d48fe3c1573eaff5ed32d6
Mark Godfrey <mark.godfrey@oracle.com>
2018-02-19 13:08:33 +0000

    Change LB node agent to use TLS connection.

--- api/agent/lb/agent.go
+++ api/agent/lb/agent.go
@@ -4,10 +4,18 @@ package lb
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	"github.com/fnproject/fn/api/agent"
 	pb "github.com/fnproject/fn/api/agent/grpc"
@@ -18,12 +26,18 @@ import (
 type lbAgent struct {
 	runnerAddress  string
 	delegatedAgent agent.Agent
+	cert           string
+	key            string
+	ca             string
 }
 
 func New(runnerAddress string, agent agent.Agent, cert string, key string, ca string) agent.Agent {
 	return &lbAgent{
 		runnerAddress:  runnerAddress,
 		delegatedAgent: agent,
+		cert:           cert,
+		key:            key,
+		ca:             ca,
 	}
 }
 
@@ -47,11 +61,19 @@ func (a *lbAgent) Submit(call agent.Call) error {
 	// Runner URL won't be a config option here, but will be obtained from
 	// the node pool manager
 
-	conn, err := grpc.Dial(a.runnerAddress, grpc.WithInsecure())
+	// Create a connection with the TLS credentials
+	ctx := context.Background()
+	creds, err := createCredentials(a.cert, a.key, a.ca)
 	if err != nil {
-		logrus.WithError(err).Error("Unable to connect to runner node")
+		logrus.WithError(err).Error("Unable to create credentials to connect to runner node")
 		return err
 	}
+	conn, err := blockingDial(ctx, a.runnerAddress, creds)
+	if err != nil {
+		logrus.WithError(err).Error("Unable to connect to runner node")
+		return fmt.Errorf("could not dial %s: %s", a.runnerAddress, err)
+	}
+
 	defer conn.Close()
 
 	c := pb.NewRunnerProtocolClient(conn)
@@ -105,4 +127,96 @@ func (a *lbAgent) AddCallListener(fnext.CallListener) {
 func (a *lbAgent) Enqueue(context.Context, *models.Call) error {
 	logrus.Fatal("Enqueue not implemented. Panicking.")
 	return nil
+}
+
+func createCredentials(certPath string, keyPath string, caCertPath string) (credentials.TransportCredentials, error) {
+	// Load the client certificates from disk
+	certificate, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not load client key pair: %s", err)
+	}
+
+	// Create a certificate pool from the certificate authority
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read ca certificate: %s", err)
+	}
+
+	// Append the certificates from the CA
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		return nil, errors.New("failed to append ca certs")
+	}
+
+	return credentials.NewTLS(&tls.Config{
+		ServerName:   "127.0.0.1", // NOTE: this is required!
+		Certificates: []tls.Certificate{certificate},
+		RootCAs:      certPool,
+	}), nil
+}
+
+// the standard grpc dial does not block on connection failures and hence completely hides all TLS errors
+func blockingDial(ctx context.Context, address string, creds credentials.TransportCredentials, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	// grpc.Dial doesn't provide any information on permanent connection errors (like
+	// TLS handshake failures). So in order to provide good error messages, we need a
+	// custom dialer that can provide that info. That means we manage the TLS handshake.
+	result := make(chan interface{}, 1)
+
+	writeResult := func(res interface{}) {
+		// non-blocking write: we only need the first result
+		select {
+		case result <- res:
+		default:
+		}
+	}
+
+	dialer := func(address string, timeout time.Duration) (net.Conn, error) {
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+		conn, err := (&net.Dialer{Cancel: ctx.Done()}).Dial("tcp", address)
+		if err != nil {
+			writeResult(err)
+			return nil, err
+		}
+		if creds != nil {
+			conn, _, err = creds.ClientHandshake(ctx, address, conn)
+			if err != nil {
+				writeResult(err)
+				return nil, err
+			}
+		}
+		return conn, nil
+	}
+
+	// Even with grpc.FailOnNonTempDialError, this call will usually timeout in
+	// the face of TLS handshake errors. So we can't rely on grpc.WithBlock() to
+	// know when we're done. So we run it in a goroutine and then use result
+	// channel to either get the channel or fail-fast.
+	go func() {
+		opts = append(opts,
+			grpc.WithBlock(),
+			grpc.FailOnNonTempDialError(true),
+			grpc.WithDialer(dialer),
+			grpc.WithInsecure(), // we are handling TLS, so tell grpc not to
+		)
+		conn, err := grpc.DialContext(ctx, address, opts...)
+		var res interface{}
+		if err != nil {
+			res = err
+		} else {
+			res = conn
+		}
+		writeResult(res)
+	}()
+
+	select {
+	case res := <-result:
+		if conn, ok := res.(*grpc.ClientConn); ok {
+			return conn, nil
+		} else {
+			return nil, res.(error)
+		}
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
 }
