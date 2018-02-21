065546d5caa572972077478b6dce4a3e20724dbf
gerardo_viedma <gerardo.viedma.nunez@oracle.com>
2018-02-21 10:14:59 +0000

    splits grpc and api interfaces for capacity manager

--- poolmanager/api.go
+++ poolmanager/api.go
@@ -1,20 +1,64 @@
 package poolmanager
 
 import (
-	"context"
 	"sync"
 	"time"
 
 	model "github.com/fnproject/fn/poolmanager/grpc"
-	ptypes "github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes"
+
 	"github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
 )
 
-type Client interface {
-	AdvertiseCapacity(snapshots *model.CapacitySnapshotList) error
-	GetLBGroupMembership(id *model.LBGroupId) (*model.LBGroupMembership, error)
-	Shutdown() error
+type NodePoolManager interface {
+	ScheduleUpdates(lbID string, agg CapacityAggregator, period time.Duration)
+	GetLBGroup(lbgID string) ([]string, error)
+}
+
+type remoteNodePoolManager struct {
+	serverAddr string
+	client     remoteClient
+}
+
+func NewNodePoolManager(npmAddr string) NodePoolManager {
+	// TODO support reconnects
+	c, e := newRemoteClient(npmAddr)
+	if e != nil {
+		logrus.Error("Failed to connect to the node pool manager for sending capacity update")
+	}
+	return &remoteNodePoolManager{serverAddr: npmAddr, client: c}
+}
+
+func (npm *remoteNodePoolManager) ScheduleUpdates(lbID string, agg CapacityAggregator, period time.Duration) {
+	ticker := time.NewTicker(period)
+	go func() {
+		for _ = range ticker.C {
+			var snapshots []*model.CapacitySnapshot
+			agg.Iterate(func(lbgID string, e *CapacityEntry) {
+				snapshot := &model.CapacitySnapshot{GroupId: &model.LBGroupId{Id: lbgID}, MemMbTotal: e.TotalMemoryMb}
+				snapshots = append(snapshots, snapshot)
+			})
+			logrus.Debugf("Advertising new capacity snapshot %+v", snapshots)
+			npm.client.AdvertiseCapacity(&model.CapacitySnapshotList{
+				Snapshots: snapshots,
+				LbId:      lbID,
+				Ts:        ptypes.TimestampNow(),
+			})
+		}
+	}()
+}
+
+func (npm *remoteNodePoolManager) GetLBGroup(lbgID string) ([]string, error) {
+	m, err := npm.client.GetLBGroup(&model.LBGroupId{Id: lbgID})
+	if err != nil {
+		return nil, err
+	}
+
+	runnerList := make([]string, len(m.GetRunners()))
+	for i, r := range m.GetRunners() {
+		runnerList[i] = r.Address
+	}
+	return runnerList, nil
 }
 
 //CapacityAggregator exposes the method to manage capacity calculation
@@ -31,52 +75,6 @@ type CapacityEntry struct {
 type inMemoryAggregator struct {
 	capacity map[string]*CapacityEntry
 	capMtx   *sync.RWMutex
-}
-type grpcPoolManagerClient struct {
-	scaler  model.NodePoolScalerClient
-	manager model.RunnerManagerClient
-	conn    *grpc.ClientConn
-}
-
-func CapacityUpdatesSchedule(serverAddr, lbID string, agg CapacityAggregator, period time.Duration) {
-	// TODO support reconnects
-	c, e := NewClient(serverAddr)
-	if e != nil {
-		logrus.Error("Failed to connect to the node pool manager for sending capacity update")
-		return
-	}
-
-	ticker := time.NewTicker(period)
-	go func() {
-		for _ = range ticker.C {
-			var snapshots []*model.CapacitySnapshot
-			agg.Iterate(func(lbgID string, e *CapacityEntry) {
-				snapshot := &model.CapacitySnapshot{GroupId: &model.LBGroupId{Id: lbgID}, MemMbTotal: e.TotalMemoryMb}
-				snapshots = append(snapshots, snapshot)
-			})
-			logrus.Debugf("Advertising new capacity snapshot %+v", snapshots)
-			c.AdvertiseCapacity(&model.CapacitySnapshotList{
-				Snapshots: snapshots,
-				LbId:      lbID,
-				Ts:        ptypes.TimestampNow(),
-			})
-		}
-	}()
-}
-
-func NewClient(serverAddr string) (Client, error) {
-	opts := []grpc.DialOption{
-		grpc.WithInsecure()}
-	conn, err := grpc.Dial(serverAddr, opts...)
-	if err != nil {
-		return nil, err
-	}
-
-	return &grpcPoolManagerClient{
-		scaler:  model.NewNodePoolScalerClient(conn),
-		manager: model.NewRunnerManagerClient(conn),
-		conn:    conn,
-	}, nil
 }
 
 //NewCapacityAggregator return a CapacityAggregator
@@ -115,21 +113,4 @@ func (a *inMemoryAggregator) Iterate(fn func(string, *CapacityEntry)) {
 	for k, v := range a.capacity {
 		fn(k, v)
 	}
-}
-
-func (c *grpcPoolManagerClient) AdvertiseCapacity(snapshots *model.CapacitySnapshotList) error {
-	_, err := c.scaler.AdvertiseCapacity(context.Background(), snapshots)
-	if err != nil {
-		logrus.Fatalf("Failed to push snapshots %v", err)
-		return err
-	}
-	return nil
-}
-
-func (c *grpcPoolManagerClient) GetLBGroupMembership(id *model.LBGroupId) (*model.LBGroupMembership, error) {
-	return c.manager.GetLBGroup(context.Background(), id)
-}
-
-func (c *grpcPoolManagerClient) Shutdown() error {
-	return c.conn.Close()
 }
---
+++ poolmanager/client.go
@@ -0,0 +1,53 @@
+package poolmanager
+
+import (
+	"context"
+
+	model "github.com/fnproject/fn/poolmanager/grpc"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+type remoteClient interface {
+	AdvertiseCapacity(snapshots *model.CapacitySnapshotList) error
+	GetLBGroup(id *model.LBGroupId) (*model.LBGroupMembership, error)
+	Shutdown() error
+}
+
+type grpcPoolManagerClient struct {
+	scaler  model.NodePoolScalerClient
+	manager model.RunnerManagerClient
+	conn    *grpc.ClientConn
+}
+
+func newRemoteClient(serverAddr string) (remoteClient, error) {
+	opts := []grpc.DialOption{
+		grpc.WithInsecure()}
+	conn, err := grpc.Dial(serverAddr, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &grpcPoolManagerClient{
+		scaler:  model.NewNodePoolScalerClient(conn),
+		manager: model.NewRunnerManagerClient(conn),
+		conn:    conn,
+	}, nil
+}
+
+func (c *grpcPoolManagerClient) AdvertiseCapacity(snapshots *model.CapacitySnapshotList) error {
+	_, err := c.scaler.AdvertiseCapacity(context.Background(), snapshots)
+	if err != nil {
+		logrus.Fatalf("Failed to push snapshots %v", err)
+		return err
+	}
+	return nil
+}
+
+func (c *grpcPoolManagerClient) GetLBGroup(id *model.LBGroupId) (*model.LBGroupMembership, error) {
+	return c.manager.GetLBGroup(context.Background(), id)
+}
+
+func (c *grpcPoolManagerClient) Shutdown() error {
+	return c.conn.Close()
+}
