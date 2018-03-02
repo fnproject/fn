503d578709d6c487b6c9366b7cd37821a4acfa98
Dario Domizioli <dario.domizioli@oracle.com>
2018-03-02 16:57:14 +0000

    Lots of fixes to Pure Runner - concurrency woes with errors and cancellations

--- api/agent/pure_runner.go
+++ api/agent/pure_runner.go
@@ -11,6 +11,7 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -25,39 +26,45 @@ import (
 	"google.golang.org/grpc/peer"
 )
 
-type pureRunner struct {
-	gRPCServer *grpc.Server
-	listen     string
-	a          Agent
-	inflight   int32
-}
-
-type writerFacade struct {
+// callHandle represents the state of the call as handled by the pure runner, and additionally it implements the
+// interface of http.ResponseWriter so that it can be used for streaming the output back.
+type callHandle struct {
 	engagement    runner.RunnerProtocol_EngageServer
+	c             *call // the agent's version of call
+	input         io.WriteCloser
+	started       bool
+	// As the state can be set and checked by both goroutines handling this state, we need a mutex.
+	stateMutex    sync.Mutex
+	// Timings, for metrics:
+	receivedTime  strfmt.DateTime // When was the call received?
+	allocatedTime strfmt.DateTime // When did we finish allocating the slot?
+	// Last communication error on the stream (if any). This basically acts as a cancellation flag too.
+	streamError   error
+	// For implementing http.ResponseWriter:
 	outHeaders    http.Header
 	outStatus     int
 	headerWritten bool
 }
 
-func (w *writerFacade) Header() http.Header {
-	return w.outHeaders
+func (ch *callHandle) Header() http.Header {
+	return ch.outHeaders
 }
 
-func (w *writerFacade) WriteHeader(status int) {
-	w.outStatus = status
-	w.commitHeaders()
+func (ch *callHandle) WriteHeader(status int) {
+	ch.outStatus = status
+	ch.commitHeaders()
 }
 
-func (w *writerFacade) commitHeaders() {
-	if w.headerWritten {
-		return
+func (ch *callHandle) commitHeaders() error {
+	if ch.headerWritten {
+		return nil
 	}
-	w.headerWritten = true
-	logrus.Infof("Committing call result with status %d and headers %v", w.outStatus, w.outHeaders)
+	ch.headerWritten = true
+	logrus.Infof("Committing call result with status %d and headers %v", ch.outStatus, ch.outHeaders)
 
 	var outHeaders []*runner.HttpHeader
 
-	for h, vals := range w.outHeaders {
+	for h, vals := range ch.outHeaders {
 		for _, v := range vals {
 			outHeaders = append(outHeaders, &runner.HttpHeader{
 				Key:   h,
@@ -66,32 +73,54 @@ func (w *writerFacade) commitHeaders() {
 		}
 	}
 
-	logrus.Info("Sending call result start message")
+	// Only write if we are not in an error situation. If we cause a stream error, then record that but don't cancel
+	// the call: basically just blackhole the output and return the write error to cause Submit to fail properly.
+	ch.stateMutex.Lock()
+	defer ch.stateMutex.Unlock()
+	err := ch.streamError
+	if err != nil {
+		return fmt.Errorf("Bailing out because of communication error: %v", ch.streamError)
+	}
 
-	err := w.engagement.Send(&runner.RunnerMsg{
+	logrus.Info("Sending call result start message")
+	err = ch.engagement.Send(&runner.RunnerMsg{
 		Body: &runner.RunnerMsg_ResultStart{
 			ResultStart: &runner.CallResultStart{
 				Meta: &runner.CallResultStart_Http{
 					Http: &runner.HttpRespMeta{
 						Headers:    outHeaders,
-						StatusCode: int32(w.outStatus),
+						StatusCode: int32(ch.outStatus),
 					},
 				},
 			},
 		},
 	})
-
 	if err != nil {
 		logrus.Errorf("Error sending call result: %v", err)
-		return
+		ch.streamError = err
+		return err
 	}
 	logrus.Info("Sent call result message")
+	return nil
 }
 
-func (w *writerFacade) Write(data []byte) (int, error) {
+func (ch *callHandle) Write(data []byte) (int, error) {
+	err := ch.commitHeaders()
+	if err != nil {
+		return 0, fmt.Errorf("Error sending data: %v", err)
+	}
+
+	// Only write if we are not in an error situation. If we cause a stream error, then record that but don't cancel
+	// the call: basically just blackhole the output and return the write error to cause Submit to fail properly.
+	ch.stateMutex.Lock()
+	defer ch.stateMutex.Unlock()
+	err = ch.streamError
+	if err != nil {
+		return 0, fmt.Errorf("Bailing out because of communication error: %v", ch.streamError)
+	}
+
 	logrus.Infof("Sending call response data %d bytes long", len(data))
-	w.commitHeaders()
-	err := w.engagement.Send(&runner.RunnerMsg{
+	err = ch.engagement.Send(&runner.RunnerMsg{
 		Body: &runner.RunnerMsg_Data{
 			Data: &runner.DataFrame{
 				Data: data,
@@ -99,17 +128,29 @@ func (w *writerFacade) Write(data []byte) (int, error) {
 			},
 		},
 	})
-
 	if err != nil {
+		ch.streamError = err
 		return 0, fmt.Errorf("Error sending data: %v", err)
 	}
 	return len(data), nil
 }
 
-func (w *writerFacade) Close() error {
+func (ch *callHandle) Close() error {
+	err := ch.commitHeaders()
+	if err != nil {
+		return fmt.Errorf("Error sending close frame: %v", err)
+	}
+
+	// Only write if we are not in an error situation. If we cause a stream error, then record that but don't cancel
+	// the call: basically just blackhole the output and return the write error to cause the caller to fail properly.
+	ch.stateMutex.Lock()
+	defer ch.stateMutex.Unlock()
+	err = ch.streamError
+	if err != nil {
+		return fmt.Errorf("Bailing out because of communication error: %v", ch.streamError)
+	}
 	logrus.Info("Sending call response data end")
-	w.commitHeaders()
-	err := w.engagement.Send(&runner.RunnerMsg{
+	err = ch.engagement.Send(&runner.RunnerMsg{
 		Body: &runner.RunnerMsg_Data{
 			Data: &runner.DataFrame{
 				Eof: true,
@@ -123,24 +164,63 @@ func (w *writerFacade) Close() error {
 	return nil
 }
 
-type callState struct {
-	c             *call // the agent's version of call
-	w             *writerFacade
-	input         io.WriteCloser
-	started       bool
-	receivedTime  strfmt.DateTime // When was the call received?
-	allocatedTime strfmt.DateTime // When did we finish allocating the slot?
-	streamError   error           // Last communication error on the stream
+// cancel implements the logic for cancelling the execution of a call based on what the state in the handle is.
+func (ch *callHandle) cancel(ctx context.Context, err error) {
+	ch.stateMutex.Lock()
+	defer ch.stateMutex.Unlock()
+
+	// Do not double-cancel.
+	if ch.streamError != nil {
+		return
+	}
+
+	// First, record that there has been an error.
+	ch.streamError = err
+	// Caller may have died or disconnected. The behaviour here depends on the state of the call.
+	// If the call was placed (i.e. GetCall was called and a slot reserved) we need to handle it...
+	if ch.c != nil {
+		// If we've actually started the call we're in the middle of an execution with i/o going back and forth.
+		// This is hard to stop. Side effects can be occurring at any point. However, at least we should stop
+		// the i/o flow. Recording the stream error in the handle should have stopped the output, but we also
+		// want to stop any input being sent through, so we close the input stream and let the function
+		// probably crash out. If it doesn't crash out, well, it means the function doesn't handle i/o errors
+		// properly and it will hang there until the timeout, then it'll be killed properly by the timeout
+		// handling in Submit.
+		if ch.started {
+			ch.input.Close()
+		} else {
+			// If we haven't started but we have allocated a reserved slot, we need to close it and release it.
+			if ch.c.reservedSlot != nil {
+				ch.c.reservedSlot.Close(ctx)
+			}
+		}
+	}
 }
 
-func (pr *pureRunner) handleData(ctx context.Context, data *runner.DataFrame, state *callState) error {
+type pureRunner struct {
+	gRPCServer *grpc.Server
+	listen     string
+	a          Agent
+	inflight   int32
+}
+
+func (pr *pureRunner) ensureFunctionIsRunning(state *callHandle) {
+	// Only start it once!
+	state.stateMutex.Lock()
+	defer state.stateMutex.Unlock()
 	if !state.started {
 		state.started = true
 		go func() {
 			err := pr.a.Submit(state.c)
 			if err != nil {
-				if state.streamError == nil { // If we can still write back...
-					err2 := state.w.engagement.Send(&runner.RunnerMsg{
+				// In this case the function has failed for a legitimate reason. We send a call failed message if we
+				// can. If there's a streaming error doing that then we are basically in the "double exception" case
+				// and who knows what's best to do. Submit has already finished so we don't need to cancel... but at
+				// least we should set streamError if it's not set.
+				state.stateMutex.Lock()
+				defer state.stateMutex.Unlock()
+				if state.streamError == nil {
+					err2 := state.engagement.Send(&runner.RunnerMsg{
 						Body: &runner.RunnerMsg_Finished{&runner.CallFinished{
 							Success: false,
 							Details: fmt.Sprintf("%v", err),
@@ -152,10 +232,16 @@ func (pr *pureRunner) handleData(ctx context.Context, data *runner.DataFrame, st
 				return
 			}
 			// First close the writer, then send the call finished message
-			err = state.w.Close()
+			err = state.Close()
 			if err != nil {
-				if state.streamError == nil { // If we can still write back...
-					err2 := state.w.engagement.Send(&runner.RunnerMsg{
+				// If we fail to close the writer we need to communicate back that the function has failed; if there's
+				// a streaming error doing that then we are basically in the "double exception" case and who knows
+				// what's best to do. Submit has already finished so we don't need to cancel... but at least we should
+				// set streamError if it's not set.
+				state.stateMutex.Lock()
+				defer state.stateMutex.Unlock()
+				if state.streamError == nil {
+					err2 := state.engagement.Send(&runner.RunnerMsg{
 						Body: &runner.RunnerMsg_Finished{&runner.CallFinished{
 							Success: false,
 							Details: fmt.Sprintf("%v", err),
@@ -166,8 +252,13 @@ func (pr *pureRunner) handleData(ctx context.Context, data *runner.DataFrame, st
 				}
 				return
 			}
-			if state.streamError == nil { // If we can still write back...
-				err2 := state.w.engagement.Send(&runner.RunnerMsg{
+			// At this point everything should have worked. Send a successful message... and if that runs afoul of a
+			// stream error, well, we're in a bit of trouble. Everything has finished, so there is nothing to cancel
+			// and we just give up, but at least we set streamError.
+			state.stateMutex.Lock()
+			defer state.stateMutex.Unlock()
+			if state.streamError == nil {
+				err2 := state.engagement.Send(&runner.RunnerMsg{
 					Body: &runner.RunnerMsg_Finished{&runner.CallFinished{
 						Success: true,
 						Details: state.c.Model().ID,
@@ -178,20 +269,29 @@ func (pr *pureRunner) handleData(ctx context.Context, data *runner.DataFrame, st
 			}
 		}()
 	}
+}
 
-	if len(data.Data) > 0 {
-		_, err := state.input.Write(data.Data)
-		if err != nil {
-			return err
+func (pr *pureRunner) handleData(ctx context.Context, data *runner.DataFrame, state *callHandle) error {
+	pr.ensureFunctionIsRunning(state)
+
+	// Only push the input if we're in a non-error situation
+	state.stateMutex.Lock()
+	defer state.stateMutex.Unlock()
+	if state.streamError == nil {
+		if len(data.Data) > 0 {
+			_, err := state.input.Write(data.Data)
+			if err != nil {
+				return err
+			}
 		}
-	}
-	if data.Eof {
-		state.input.Close()
+		if data.Eof {
+			state.input.Close()
+		}
 	}
 	return nil
 }
 
-func (pr *pureRunner) handleTryCall(ctx context.Context, tc *runner.TryCall, state *callState) error {
+func (pr *pureRunner) handleTryCall(ctx context.Context, tc *runner.TryCall, state *callHandle) error {
 	var c models.Call
 	err := json.Unmarshal([]byte(tc.ModelsCallJson), &c)
 	if err != nil {
@@ -201,7 +301,7 @@ func (pr *pureRunner) handleTryCall(ctx context.Context, tc *runner.TryCall, sta
 
 	state.receivedTime = strfmt.DateTime(time.Now())
 	var w http.ResponseWriter
-	w = state.w
+	w = state
 	inR, inW := io.Pipe()
 	agent_call, err := pr.a.GetCall(FromModelAndInput(&c, inR), WithWriter(w), WithReservedSlot(ctx, nil))
 	if err != nil {
@@ -231,16 +331,15 @@ func (pr *pureRunner) Engage(engagement runner.RunnerProtocol_EngageServer) erro
 		logrus.Info("MD is ", md)
 	}
 
-	var state = callState{
-		c: nil,
-		w: &writerFacade{
-			engagement:    engagement,
-			outHeaders:    make(http.Header),
-			outStatus:     200,
-			headerWritten: false,
-		},
-		started:     false,
-		streamError: nil,
+	var state = callHandle{
+		engagement:    engagement,
+		c:             nil,
+		input:         nil,
+		started:       false,
+		streamError:   nil,
+		outHeaders:    make(http.Header),
+		outStatus:     200,
+		headerWritten: false,
 	}
 
 	grpc.EnableTracing = false
@@ -248,15 +347,9 @@ func (pr *pureRunner) Engage(engagement runner.RunnerProtocol_EngageServer) erro
 	for {
 		msg, err := engagement.Recv()
 		if err != nil {
-			state.streamError = err
-			// Caller may have died. Entirely kill the container by pushing an
-			// eof on the input, even for hot. This ensures that the hot
-			// container is not stuck in a state where it is still expecting
-			// half the input of the previous call. The error this will likely
-			// cause will then release the slot.
-			if state.c != nil && state.c.reservedSlot != nil {
-				state.input.Close()
-			}
+			// In this case the connection has dropped or there's something bad happening. We know we can't even send
+			// a message back. Cancel the call, all bets are off.
+			state.cancel(engagement.Context(), err)
 			return err
 		}
 
@@ -264,42 +357,60 @@ func (pr *pureRunner) Engage(engagement runner.RunnerProtocol_EngageServer) erro
 
 		case *runner.ClientMsg_Try:
 			err := pr.handleTryCall(engagement.Context(), body.Try, &state)
+			// At the stage of TryCall, there is only one thread running and nothing has happened yet so there should
+			// not be a streamError. We can handle `err` by sending a message back and cancelling the call because
+			// the slot may need to be released. If we cause a stream error by sending the message, we are in a "double
+			// exception" case and we might as well cancel the call with the original error, so we can ignore the error
+			// from Send.
 			if err != nil {
-				if state.streamError == nil { // If we can still write back...
-					err2 := engagement.Send(&runner.RunnerMsg{
-						Body: &runner.RunnerMsg_Acknowledged{&runner.CallAcknowledged{
-							Committed: false,
-							Details:   fmt.Sprintf("%v", err),
-						}}})
-					if err2 != nil {
-						state.streamError = err2
-					}
-				}
+				_ = engagement.Send(&runner.RunnerMsg{
+					Body: &runner.RunnerMsg_Acknowledged{&runner.CallAcknowledged{
+						Committed: false,
+						Details:   fmt.Sprintf("%v", err),
+					}}})
+				state.cancel(engagement.Context(), err)
 				return err
-			} else {
-				if state.streamError == nil { // If we can still write back...
-					err2 := engagement.Send(&runner.RunnerMsg{
-						Body: &runner.RunnerMsg_Acknowledged{&runner.CallAcknowledged{
-							Committed:             true,
-							Details:               state.c.Model().ID,
-							SlotAllocationLatency: time.Time(state.allocatedTime).Sub(time.Time(state.receivedTime)).String(),
-						}}})
-					if err2 != nil {
-						state.streamError = err2
-						return err2
-					}
-				}
+			}
+			// If we succeed in creating the call, but we get a stream error sending a message back, we must cancel
+			// the call because we've probably lost the connection.
+			err = engagement.Send(&runner.RunnerMsg{
+				Body: &runner.RunnerMsg_Acknowledged{&runner.CallAcknowledged{
+					Committed:             true,
+					Details:               state.c.Model().ID,
+					SlotAllocationLatency: time.Time(state.allocatedTime).Sub(time.Time(state.receivedTime)).String(),
+				}}})
+			if err != nil {
+				state.cancel(engagement.Context(), err)
+				return err
 			}
 
 		case *runner.ClientMsg_Data:
-			// TODO If it's the first one, actually start the call. Then stream into current call.
 			err := pr.handleData(engagement.Context(), body.Data, &state)
 			if err != nil {
-				// What do we do here?!?
+				// If this happens, then we couldn't write into the input. The state of the function is inconsistent and
+				// therefore we need to cancel. We also need to communicate back that the function has failed; that
+				// could also run afoul of a stream error, but at that point we don't care, just cancel the call with
+				// the original error.
+				_ = state.engagement.Send(&runner.RunnerMsg{
+					Body: &runner.RunnerMsg_Finished{&runner.CallFinished{
+						Success: false,
+						Details: fmt.Sprintf("%v", err),
+					}}})
+				state.cancel(engagement.Context(), err)
 				return err
 			}
 		default:
-			return fmt.Errorf("Unrecognized or unhandled message in receive loop")
+			err := fmt.Errorf("Catastrophic failure in communication with function runner")
+			// This is essentially a panic. Try to communicate back that the call has failed, and bail out; that
+			// could also run afoul of a stream error, but at that point we don't care, just cancel the call with
+			// the catastrophic error.
+			_ = state.engagement.Send(&runner.RunnerMsg{
+				Body: &runner.RunnerMsg_Finished{&runner.CallFinished{
+					Success: false,
+					Details: fmt.Sprintf("%v", err),
+				}}})
+			state.cancel(engagement.Context(), err)
+			return err
 		}
 	}
 }
@@ -320,7 +431,7 @@ func (pr *pureRunner) Start() error {
 	if err := pr.gRPCServer.Serve(lis); err != nil {
 		return fmt.Errorf("grpc serve error: %s", err)
 	}
-	return nil
+	return err
 }
 
 func CreatePureRunner(addr string, a Agent, cert string, key string, ca string) (*pureRunner, error) {
