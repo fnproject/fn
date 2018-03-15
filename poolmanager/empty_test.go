71b761e2ad43add0a3a06c91435962fe615d628a
gerardo_viedma <gerardo.viedma.nunez@oracle.com>
2018-03-15 15:58:37 +0000

    removes poolmanager

--- poolmanager/empty_test.go
+++
@@ -1,9 +0,0 @@
-package poolmanager
-
-import "testing"
-
-// empty test file as a workaround for build requiring go source files in top-level package
-// TODO remove with the rest of poolmanager
-func TestToRemove(t *testing.T) {
-
-}
