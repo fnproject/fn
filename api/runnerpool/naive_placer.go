3229da29a03a4f0324e28fd9b020097914811e4c
Tolga Ceylan <tolga.ceylan@gmail.com>
2018-04-13 12:40:07 -0700

    fn: LB ch and naive fixes
    *) Naive is now a naive RR algorithm.
    *) Both now checks for ctx/timeout in each attempt.

--- api/runnerpool/naive_placer.go
+++ api/runnerpool/naive_placer.go
@@ -2,6 +2,7 @@ package runnerpool
 
 import (
 	"context"
+	"sync/atomic"
 	"time"
 
 	"github.com/fnproject/fn/api/common"
@@ -15,51 +16,61 @@ const (
 	retryWaitInterval = 10 * time.Millisecond
 )
 
-type naivePlacer struct{}
+type naivePlacer struct {
+	rrIndex uint64
+}
 
 func NewNaivePlacer() Placer {
-	logrus.Info("Creating new naive runnerpool placer")
-	return &naivePlacer{}
+	rrIndex := uint64(time.Now().Nanosecond())
+	logrus.Infof("Creating new naive runnerpool placer rrIndex=%d", rrIndex)
+	return &naivePlacer{
+		rrIndex: rrIndex,
+	}
 }
 
 func (sp *naivePlacer) PlaceCall(rp RunnerPool, ctx context.Context, call RunnerCall) error {
 	timeout := time.After(call.LbDeadline().Sub(time.Now()))
 
 	for {
+		runners, err := rp.Runners(call)
+		if err != nil {
+			logrus.WithError(err).Error("Failed to find runners for call")
+		} else {
+			for j := 0; j < len(runners); j++ {
+
+				select {
+				case <-ctx.Done():
+					return models.ErrCallTimeoutServerBusy
+				case <-timeout:
+					return models.ErrCallTimeoutServerBusy
+				default:
+				}
+
+				i := atomic.AddUint64(&sp.rrIndex, uint64(1))
+				r := runners[int(i)%len(runners)]
+
+				placed, err := r.TryExec(ctx, call)
+				if err != nil {
+					logrus.WithError(err).Error("Failed during call placement")
+				}
+				if placed {
+					return err
+				}
+			}
+		}
+
+		remaining := call.LbDeadline().Sub(time.Now())
+		if remaining <= 0 {
+			return models.ErrCallTimeoutServerBusy
+		}
+
+		// backoff
 		select {
 		case <-ctx.Done():
 			return models.ErrCallTimeoutServerBusy
 		case <-timeout:
 			return models.ErrCallTimeoutServerBusy
-		default:
-			runners, err := rp.Runners(call)
-			if err != nil {
-				logrus.WithError(err).Error("Failed to find runners for call")
-			} else {
-				for _, r := range runners {
-					placed, err := r.TryExec(ctx, call)
-					if err != nil {
-						logrus.WithError(err).Error("Failed during call placement")
-					}
-					if placed {
-						return err
-					}
-				}
-			}
-
-			remaining := call.LbDeadline().Sub(time.Now())
-			if remaining <= 0 {
-				return models.ErrCallTimeoutServerBusy
-			}
-
-			// backoff
-			select {
-			case <-ctx.Done():
-				return models.ErrCallTimeoutServerBusy
-			case <-timeout:
-				return models.ErrCallTimeoutServerBusy
-			case <-time.After(common.MinDuration(retryWaitInterval, remaining)):
-			}
+		case <-time.After(common.MinDuration(retryWaitInterval, remaining)):
 		}
 	}
 }
