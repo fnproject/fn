d7fa116382c6b515609c57ba5016a41c776ac25d
Tolga Ceylan <tolga.ceylan@gmail.com>
2018-06-04 11:27:20 -0700

    fn: LB agent: reduce 'Too Busy' error logs
    With this PR, runner client translates too busy errors
    from gRPC session and runner itself into Fn error type.
    Placers now ignore this error message to reduce unnecessary
    logging.

--- api/runnerpool/naive_placer.go
+++ api/runnerpool/naive_placer.go
@@ -46,7 +46,7 @@ func (sp *naivePlacer) PlaceCall(rp RunnerPool, ctx context.Context, call Runner
 				placed, err := r.TryExec(tryCtx, call)
 				tryCancel()
 
-				if err != nil {
+				if err != nil && err != models.ErrCallTimeoutServerBusy {
 					logrus.WithError(err).Error("Failed during call placement")
 				}
 				if placed {
