fce1e5474610dc672ab8241a3a38ec7099d7a482
Tolga Ceylan <tolga.ceylan@gmail.com>
2018-06-08 15:57:06 -0700

    fn: remove dead code in static pool (#1052)
    Static pool is oriented for testing/basic usage and
    as it's name implies it is a static pool. Therefore,
    removing unnecessary/dead code.

--- api/agent/static_pool_test.go
+++ api/agent/static_pool_test.go
@@ -47,6 +47,11 @@ func TestNewStaticPool(t *testing.T) {
 	if len(runners) != len(addrs) {
 		t.Fatalf("Invalid number of runners %v", len(runners))
 	}
+
+	err = np.Shutdown(context.Background())
+	if err != ErrorGarbanzoBeans {
+		t.Fatalf("Expected garbanzo beans error from shutdown %v", err)
+	}
 }
 
 func TestEmptyPool(t *testing.T) {
@@ -60,129 +65,8 @@ func TestEmptyPool(t *testing.T) {
 		t.Fatalf("Invalid number of runners %v", len(runners))
 	}
 
-	err = np.AddRunner("127.0.0.1:8082")
-	if err != nil {
-		t.Fatalf("Failed to add runner %v", err)
-	}
-
-	runners, err = np.Runners(nil)
-	if err != nil {
-		t.Fatalf("Failed to list runners %v", err)
-	}
-	if len(runners) != 1 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
-	}
-
-	err = np.Shutdown(context.Background())
-	if err != ErrorGarbanzoBeans {
-		t.Fatalf("Expected garbanzo beans error from shutdown %v", err)
-	}
-
-	runners, err = np.Runners(nil)
-	if err == nil {
-		t.Fatalf("Should fail to list runners (shutdown)")
-	}
-	if len(runners) != 0 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
-	}
-}
-
-func TestAddNodeToPool(t *testing.T) {
-	addrs := []string{"127.0.0.1:8080", "127.0.0.1:8081"}
-	np := setupStaticPool(addrs).(*staticRunnerPool)
-
-	err := np.AddRunner("127.0.0.1:8082")
-	if err != nil {
-		t.Fatalf("Add Should not fail %v", err)
-	}
-
-	err = np.AddRunner("127.0.0.1:8082")
-	if err != ErrorPoolRunnerExists {
-		t.Fatalf("Add Should fail since duplicate %v", err)
-	}
-
-	runners, err := np.Runners(nil)
-	if err != nil {
-		t.Fatalf("Failed to list runners %v", err)
-	}
-	if len(runners) != 3 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
-	}
-
-	err = np.Shutdown(context.Background())
-	if err != ErrorGarbanzoBeans {
-		t.Fatalf("Expected garbanzo beans error from shutdown %v", err)
-	}
-
-	runners, err = np.Runners(nil)
-	if err == nil {
-		t.Fatalf("Should fail to list runners (shutdown)")
-	}
-	if len(runners) != 0 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
-	}
-}
-
-func TestRemoveNodeFromPool(t *testing.T) {
-	addrs := []string{"127.0.0.1:8080", "127.0.0.1:8081"}
-	np := setupStaticPool(addrs).(*staticRunnerPool)
-
-	np.RemoveRunner("127.0.0.1:8081")
-
-	runners, err := np.Runners(nil)
-	if err != nil {
-		t.Fatalf("Failed to list runners %v", err)
-	}
-
-	if len(runners) != 1 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
-	}
-
-	np.RemoveRunner("127.0.0.1:8081")
-
-	runners, err = np.Runners(nil)
-	if err != nil {
-		t.Fatalf("Failed to list runners %v", err)
-	}
-	if len(runners) != 1 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
-	}
-
-	np.RemoveRunner("127.0.0.1:8080")
-
-	runners, err = np.Runners(nil)
-	if err != nil {
-		t.Fatalf("Failed to list runners %v", err)
-	}
-	if len(runners) != 0 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
-	}
-
-	np.RemoveRunner("127.0.0.1:8080")
-
-	runners, err = np.Runners(nil)
-	if err != nil {
-		t.Fatalf("Failed to list runners %v", err)
-	}
-	if len(runners) != 0 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
-	}
-
-	// Let's try a double shutdown
 	err = np.Shutdown(context.Background())
 	if err != nil {
-		t.Fatalf("Not expected error from shutdown I (empty pool) %v", err)
-	}
-	err = np.Shutdown(context.Background())
-	if err != nil {
-		t.Fatalf("Not expected error from shutdown II (empty pool) %v", err)
-	}
-
-	runners, err = np.Runners(nil)
-	if err == nil {
-		t.Fatalf("Should fail to list runners (shutdown)")
-	}
-	if len(runners) != 0 {
-		t.Fatalf("Invalid number of runners %v", len(runners))
+		t.Fatalf("Unexpected error from shutdown %v", err)
 	}
 }
