7e46df994381917520b24150985b3c71013f74ed
lake-of-derams <ravi.singhal.cse.21@gmail.com>
2018-07-13 15:22:31 +0530

    try original ports

--- helpers_wercker.sh
+++ helpers_wercker.sh
@@ -42,7 +42,7 @@ function spawn_mysql {
     local PORT=$(get_port ${CONTEXT}_mysql)
     local HOST=${CONTEXT}_mysql
     local ID=$(docker run --name ${CONTEXT}_mysql \
-        -p ${PORT}:3306 \
+        -p 3306:3306 \
         -e MYSQL_DATABASE=funcs \
         -e MYSQL_ROOT_PASSWORD=root \
         --network=$DOCKER_NETWORK_NAME \
@@ -58,7 +58,7 @@ function spawn_postgres {
     local ID=$(docker run --name ${CONTEXT}_postgres \
         -e "POSTGRES_DB=funcs" \
         -e "POSTGRES_PASSWORD=root" \
-        -p ${PORT}:5432 \
+        -p 5432:5432 \
         --network=$DOCKER_NETWORK_NAME \
         -d postgres:9.3-alpine)
 
@@ -70,7 +70,7 @@ function spawn_minio {
     local PORT=$(get_port ${CONTEXT}_minio)
     local HOST=${CONTEXT}_minio
     local ID=$(docker run --name ${CONTEXT}_minio \
-        -p ${PORT}:9000 \
+        -p 9000:9000 \
         -e "MINIO_ACCESS_KEY=admin" \
         -e "MINIO_SECRET_KEY=password" \
         --network=$DOCKER_NETWORK_NAME \
