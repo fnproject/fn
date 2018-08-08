7089581c0785a8a57505c32d97b3d5c724e8ab6d
Peter Jausovec <peter.jausovec@oracle.com>
2018-08-08 12:54:54 -0700

    Add AppName to the models.Call, so we can include it in the syslog

--- api/agent/syslog_test.go
+++ api/agent/syslog_test.go
@@ -13,15 +13,16 @@ func TestSyslogFormat(t *testing.T) {
 
 	call := "12345"
 	fn := "yo/dawg"
-	app := "sup"
+	appID := "sup"
+	appName := "myapp"
 	now := time.Date(1982, 6, 25, 12, 0, 0, 0, time.UTC)
 	clock := func() time.Time { return now }
 
-	writer := newSyslogWriter(call, fn, app, syslog.LOG_ERR, &nopCloser{&b1}, &b2)
+	writer := newSyslogWriter(call, fn, appID, appName, syslog.LOG_ERR, &nopCloser{&b1}, &b2)
 	writer.clock = clock
 	writer.Write([]byte("yo"))
 
-	gold := `<11>2 1982-06-25T12:00:00Z fn - - - - call_id=12345 func_name=yo/dawg app_id=sup yo`
+	gold := `<11>2 1982-06-25T12:00:00Z fn - - - - call_id=12345 func_name=yo/dawg app_id=sup app_name=myapp yo`
 
 	if b1.String() != gold {
 		t.Fatal("syslog was not what we expected: ", b1.String())
