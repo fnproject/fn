b861f26d6a0893e034f5cb1922c73734bc77d55c
Reed Allman <reed@rdallman.com>
2018-08-09 00:57:23 -0700

    swap out for github.com/fnproject/cloudevent (#1167)
    this gets rid of the fn core specific event that was added in the previous
    commit. we may need to modify the cloudevent lib to our needs, but anyway,
    this switches over to it, we can go from here.
    
    added various TODO in the code from things that stuck out that we need to do.
    in addition to those:
    
    TODO these will interface with our own hokey protocol/cloudevent thing OK but
    we should probably swap that out, too, to be sure.
    TODO we need to figure out if we want to fill in some of the 'required' fields
    or require users to supply them. with the cli it's pretty easy, and it would
    be nice to validate those fields for sanity.

--- api/server/runner_fninvoke.go
+++ api/server/runner_fninvoke.go
@@ -2,11 +2,13 @@ package server
 
 import (
 	"bytes"
+	"io"
+	"strconv"
 
+	"github.com/fnproject/cloudevent"
 	"github.com/fnproject/fn/api"
 	"github.com/fnproject/fn/api/agent"
 	"github.com/fnproject/fn/api/common"
-	"github.com/fnproject/fn/api/event"
 	"github.com/fnproject/fn/api/models"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -23,21 +25,17 @@ func (s *Server) handleFnInvokeCall(c *gin.Context) {
 	}
 }
 
-const (
-	fnInvokeContentType = "application/cloudevents+json"
-)
-
 // handleTriggerHTTPFunctionCall2 executes the function and returns an error
 // Requires the following in the context:
 func (s *Server) handleFnInvokeCall2(c *gin.Context) error {
-	//	log := common.Logger(c.Request.Context())
-
 	fn, err := s.lbReadAccess.GetFnByID(c, c.Param(api.ParamFnID))
 	if err != nil {
 		return err
 	}
 
 	if fn.Format != models.FormatCloudEvent {
+		// XXX(reed): we could just override it, as we do currently in the /r/ cloudevent model.
+		// the blow up then gets pushed down into the container, which is arguably harder to chase (but fdks right?)
 		return models.ErrOnlyCloudEventFnsSupported
 	}
 
@@ -46,33 +44,25 @@ func (s *Server) handleFnInvokeCall2(c *gin.Context) error {
 		return err
 	}
 
-	contentType := c.ContentType()
-	if contentType != fnInvokeContentType {
-		return models.ErrUnsupportedMediaType
-	}
-
-	event := &event.Event{}
-	err = c.BindJSON(event)
+	var event cloudevent.CloudEvent
+	err = event.FromRequest(c.Request)
 	if err != nil {
-		if !models.IsAPIError(err) {
-			err = models.ErrInvalidJSON
-		}
+		// XXX(reed): we should make these API errors most likely instead of 5xx
 		handleErrorResponse(c, err)
 		return err
 	}
 
-	err = event.Validate()
-	if err != nil {
-		return err
-	}
+	// TODO if we validate we have to strictly enforce certain fields we don't
+	// really need in order to run a function (cloudEventVersion we do, but we
+	// need to probably be lax and assume 0.1 if none provided, id doesn't
+	// matter as for example, to run a function, we have call id)
 
-	s.ServeFnInvoke(c, event, fn, app)
+	s.ServeFnInvoke(c, &event, fn, app)
 
 	return nil
 }
 
-func (s *Server) ServeFnInvoke(c *gin.Context, event *event.Event, fn *models.Fn, app *models.App) error {
-	log := common.Logger(c.Request.Context())
+func (s *Server) ServeFnInvoke(c *gin.Context, event *cloudevent.CloudEvent, fn *models.Fn, app *models.App) error {
 	buf := bufPool.Get().(*bytes.Buffer)
 	buf.Reset()
 	writer := syncResponseWriter{
@@ -83,8 +73,35 @@ func (s *Server) ServeFnInvoke(c *gin.Context, event *event.Event, fn *models.Fn
 
 	call, err := s.agent.GetCall(
 		agent.WithWriter(&writer), // XXX (reed): order matters [for now]
-		agent.FromFnInvokeRequest(c.Request.Context, app, fn, event),
+		agent.FromFnInvokeRequest(c.Request.Context(), app, fn, event),
 	)
+
+	// note that underneath, since we enforced the function format to be 'cloudevent'
+	// then the container will produce a cloudevent (via the cloudevent protocol Dispatch method)
+	// that will be in the writer after Submit gets done.
+
+	err = s.agent.Submit(call)
+	if err != nil {
+		// NOTE if they cancel the request then it will stop the call (kind of cool),
+		// we could filter that error out here too as right now it yells a little
+		if err == models.ErrCallTimeoutServerBusy || err == models.ErrCallTimeout {
+			// TODO maneuver
+			// add this, since it means that start may not have been called [and it's relevant]
+			// c.Writer.Header().Add("XXX-FXLB-WAIT", time.Now().Sub(time.Time(model.CreatedAt)).String())
+		}
+		return err
+	}
+
+	// TODO they may write out a binary cloud event to the buffer, we need to
+	// handle this somehow (how do they set the headers?) but we can't set the
+	// content type here really.
+	// TODO we kinda need submit to return the event so we can do ^ b/c LAYERS
+
+	writer.Header().Set("Content-Length", strconv.Itoa(int(buf.Len())))
+	if writer.status > 0 {
+		c.Writer.WriteHeader(writer.status)
+	}
+	io.Copy(c.Writer, &writer)
 
 	return nil
 }
