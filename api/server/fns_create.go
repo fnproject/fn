d336035678c4004e3464a31abf5331efc6bdb2a8
James Jeffrey <wizardofmath@gmail.com>
2018-08-21 02:26:36 -0700

    Add annotation to trigger on create if endpoints are enabled (#1177)
    * Add annotations for creation of triggers and fns along with the test for them fixes #1178
    
    * Log errors and still return created resource for annotation failures

--- api/server/fns_create.go
+++ api/server/fns_create.go
@@ -3,12 +3,14 @@ package server
 import (
 	"net/http"
 
+	"github.com/fnproject/fn/api/common"
 	"github.com/fnproject/fn/api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func (s *Server) handleFnCreate(c *gin.Context) {
 	ctx := c.Request.Context()
+	log := common.Logger(ctx)
 
 	fn := &models.Fn{}
 	err := c.BindJSON(fn)
@@ -24,7 +26,22 @@ func (s *Server) handleFnCreate(c *gin.Context) {
 	fnCreated, err := s.datastore.InsertFn(ctx, fn)
 	if err != nil {
 		handleErrorResponse(c, err)
+		return
 	}
 
-	c.JSON(http.StatusOK, fnCreated)
+	app, err := s.datastore.GetAppByID(ctx, fnCreated.AppID)
+	if err != nil {
+		log.Debugln("Failed to lookup app.")
+		c.JSON(http.StatusOK, fnCreated)
+		return
+	}
+
+	fnAnnotated, err := s.fnAnnotator.AnnotateFn(c, app, fnCreated)
+	if err != nil {
+		log.Debugln("Failed to annotate fn")
+		c.JSON(http.StatusOK, fnCreated)
+		return
+	}
+
+	c.JSON(http.StatusOK, fnAnnotated)
 }
--- api/server/trigger_create.go
+++ api/server/trigger_create.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/fnproject/fn/api/common"
 	"github.com/fnproject/fn/api/models"
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +12,7 @@ import (
 func (s *Server) handleTriggerCreate(c *gin.Context) {
 	ctx := c.Request.Context()
 	trigger := &models.Trigger{}
+	log := common.Logger(ctx)
 
 	err := c.BindJSON(trigger)
 	if err != nil {
@@ -27,5 +30,19 @@ func (s *Server) handleTriggerCreate(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, triggerCreated)
+	app, err := s.datastore.GetAppByID(ctx, triggerCreated.AppID)
+	if err != nil {
+		log.Debugln(fmt.Errorf("unexpected error - trigger app not available: %s", err))
+		c.JSON(http.StatusOK, triggerCreated)
+		return
+	}
+
+	triggerAnnotated, err := s.triggerAnnotator.AnnotateTrigger(c, app, triggerCreated)
+	if err != nil {
+		log.Debugln("Failed to annotate trigger on cration")
+		c.JSON(http.StatusOK, triggerCreated)
+		return
+	}
+
+	c.JSON(http.StatusOK, triggerAnnotated)
 }
