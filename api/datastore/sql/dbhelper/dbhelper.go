08c64ff4fc9f45f8f02f46afb10d9acd72733eab
Reed Allman <reed@rdallman.com>
2018-10-13 03:36:58 -0700

    make dbhelper shut up about dbhelpers not being used

--- api/datastore/sql/dbhelper/dbhelper.go
+++ api/datastore/sql/dbhelper/dbhelper.go
@@ -1,4 +1,4 @@
-// dbhelpers wrap SQL and specific capabilities of an SQL db
+// Package dbhelper wraps SQL and specific capabilities of an SQL db
 package dbhelper
 
 import (
@@ -10,34 +10,34 @@ import (
 
 var sqlHelpers []Helper
 
-//Register registers a new SQL helper
+// Register registers a new SQL helper
 func Register(helper Helper) {
 	logrus.Infof("Registering sql helper '%s'", helper)
 	sqlHelpers = append(sqlHelpers, helper)
 }
 
-//Helper provides DB-specific SQL capabilities
+// Helper provides DB-specific SQL capabilities
 type Helper interface {
 	fmt.Stringer
-	//Supports indicates if this helper supports this driver name
+	// Supports indicates if this helper supports this driver name
 	Supports(driverName string) bool
-	//PreConnect calculates the connect URL for the db from a canonical URL used in Fn config
+	// PreConnect calculates the connect URL for the db from a canonical URL used in Fn config
 	PreConnect(url *url.URL) (string, error)
-	//PostCreate  Apply any configuration to the DB prior to use
+	// PostCreate  Apply any configuration to the DB prior to use
 	PostCreate(db *sqlx.DB) (*sqlx.DB, error)
-	//CheckTableExists checks if a table exists in the DB
+	// CheckTableExists checks if a table exists in the DB
 	CheckTableExists(tx *sqlx.Tx, table string) (bool, error)
-	//IsDuplicateKeyError determines if an error indicates if the prior error was caused by a duplicate key insert
+	// IsDuplicateKeyError determines if an error indicates if the prior error was caused by a duplicate key insert
 	IsDuplicateKeyError(err error) bool
 }
 
-//GetHelper returns a helper for a specific driver
+// GetHelper returns a helper for a specific driver
 func GetHelper(driverName string) (Helper, bool) {
 	for _, helper := range sqlHelpers {
 		if helper.Supports(driverName) {
 			return helper, true
 		}
-		logrus.Printf("%s does not support %s", helper, driverName)
+		logrus.Debugf("%s does not support %s", helper, driverName)
 	}
 	return nil, false
 }
