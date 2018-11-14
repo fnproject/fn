0bac40a07b98a41e988a1c6adc765f2b899e5e63
Eric Fode <eric.fode@oracle.com>
2018-11-14 10:14:35 -0800

    old change

--- api/agent/config.go
+++ api/agent/config.go
@@ -165,7 +165,6 @@ func NewConfig() (*Config, error) {
 	err = setEnvBool(err, EnvEnableNBResourceTracker, &cfg.EnableNBResourceTracker)
 	err = setEnvBool(err, EnvDisableReadOnlyRootFs, &cfg.DisableReadOnlyRootFs)
 	err = setEnvBool(err, EnvDisableDebugUserLogs, &cfg.DisableDebugUserLogs)
-	err = setEnvBool(err, EnvDisableTini, &cfg.DisableTini)
 
 	if err != nil {
 		return cfg, err
