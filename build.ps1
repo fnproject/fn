6370d96d3a81288a0142c0928709298993366c5f
Tommy Su <tommy.t.su@gmail.com>
2018-12-06 17:34:56 -0800

    Fix the powershell build script (#1343)
    * Fix the powershell build script
    
    * kick cla bot

--- build.ps1
+++ build.ps1
@@ -23,6 +23,7 @@ function quick() {
 function build () {
     docker run --rm -v ${pwd}:/go/src/github.com/fnproject/fn -w /go/src/github.com/fnproject/fn/cmd/fnserver golang:alpine go build -o fn-alpine
     docker build -t fnproject/fnserver:latest .
+}
 
 function run () {
     docker run --rm --name functions -it -v /var/run/docker.sock:/var/run/docker.sock -e FN_LOG_LEVEL=debug -e "FN_DB_URL=sqlite3:///app/data/fn.db" -v $PWD/data:/app/data -p 8080:8080 fnproject/fnserver
