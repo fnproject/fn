ac9b8d85cf01d3c1053225b7886c83221f36d6bb
Tolga Ceylan <tolga.ceylan@gmail.com>
2019-03-13 15:22:50 -0700

    add test case for errors

--- api/agent/drivers/docker/image_puller_test.go
+++ api/agent/drivers/docker/image_puller_test.go
@@ -2,6 +2,8 @@ package docker
 
 import (
 	"context"
+	"errors"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -16,12 +18,13 @@ type mockClientPuller struct {
 	dockerWrap
 
 	numCalls uint64
+	err      error
 }
 
 func (c *mockClientPuller) PullImage(opts docker.PullImageOptions, auth docker.AuthConfiguration) error {
 	time.Sleep(time.Second * 1)
 	atomic.AddUint64(&c.numCalls, uint64(1))
-	return nil
+	return c.err
 }
 
 // Lets do concurrent docker-pulls for an image with two different tags. This should results in only
@@ -48,13 +51,19 @@ func TestImagePullConcurrent1(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		go func() {
 			defer wg.Done()
-			<-puller.PullImage(ctx, &cfg, img, repo, tag1)
+			err := <-puller.PullImage(ctx, &cfg, img, repo, tag1)
+			if err != nil {
+				t.Fatalf("err received %v", err)
+			}
 		}()
 	}
 	for i := 0; i < 10; i++ {
 		go func() {
 			defer wg.Done()
-			<-puller.PullImage(ctx, &cfg, img, repo, tag2)
+			err := <-puller.PullImage(ctx, &cfg, img, repo, tag2)
+			if err != nil {
+				t.Fatalf("err received %v", err)
+			}
 		}()
 	}
 
@@ -62,6 +71,43 @@ func TestImagePullConcurrent1(t *testing.T) {
 
 	// Should be two docker-pulls
 	if mock.numCalls != 2 || ctx.Err() != nil {
+		t.Fatalf("fail numOfPulls=%d ctx=%s", mock.numCalls, ctx.Err())
+	}
+}
+
+// Lets do concurrent docker-pulls for an image but with error.
+func TestImagePullConcurrent2(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10*time.Second))
+	defer cancel()
+
+	var cli dockerClient
+	mock := mockClientPuller{err: errors.New("yogurt")}
+	cli = &mock
+
+	puller := NewImagePuller(drivers.Config{}, cli)
+
+	cfg := docker.AuthConfiguration{}
+	img := "foo"
+	repo := "zoo"
+	tag := "1.0.0"
+
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := <-puller.PullImage(ctx, &cfg, img, repo, tag)
+			if err == nil || strings.Index(err.Error(), "yogurt") == -1 {
+				t.Fatalf("Unknown err received %v", err)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	// Should be one docker-pull
+	if mock.numCalls != 1 || ctx.Err() != nil {
 		t.Fatalf("fail numOfPulls=%d ctx=%s", mock.numCalls, ctx.Err())
 	}
 }
