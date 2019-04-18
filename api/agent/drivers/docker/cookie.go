a2c69261649311613af7315151401fcb9a8c5d8e
Reed Allman <reed@rdallman.com>
2019-04-18 15:42:28 -0700

    add memory swappiness setting to 0
    without this, is vm.swappiness is not turned off, then it allows containers to
    use unlimited (up to host max) anonymous swap space on the host. this behavior
    is not what we want, particularly for getting tests to pass on everybody's dev
    environment, but we don't really want this behavior in any case.

--- api/agent/drivers/docker/cookie.go
+++ api/agent/drivers/docker/cookie.go
@@ -98,6 +98,10 @@ func (c *cookie) configureMem(log logrus.FieldLogger) {
 	c.opts.Config.Memory = mem
 	c.opts.Config.MemorySwap = mem // disables swap
 	c.opts.Config.KernelMemory = mem
+	c.opts.HostConfig.MemorySwap = mem
+	c.opts.HostConfig.KernelMemory = mem
+	var zero int64
+	c.opts.HostConfig.MemorySwappiness = &zero // disables host swap
 }
 
 func (c *cookie) configureFsSize(log logrus.FieldLogger) {
